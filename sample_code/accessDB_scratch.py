18c18921d543224e356b12222a6f29283e835f1d
mkoshkina <55889868+mkoshkina@users.noreply.github.com>
2021-10-14 12:26:18 -0400

    organize code, add length of stay

--- sample_code/accessDB_scratch.py
+++ sample_code/accessDB_scratch.py
@@ -7,12 +7,43 @@ import os
 import time
 import sqlalchemy 
 import pandas.io.sql as psql
+import argparse
 
+def read_config():
+    parser = argparse.ArgumentParser()
 
-def main(args):
-    print('postgresql://{args.user}:{args.password}@{args.host}:{args.port}/{args.database}')
-    engine = sqlalchemy.create_engine(f'postgresql://{args.user}:{args.password}@{args.host}:{args.port}/{args.database}')
+    parser.add_argument("--user", default='koshkinam', type=str, required=False, help='Postgres user')
+    # parser.add_argument("--password", default=os.environ['PGPASSWORD'], type=str, required=True, help='Postgres password')
+    parser.add_argument("--port", default=5432, type=int, help='Postgres port')
+    parser.add_argument("--host", default='db.gemini-hpc.ca', type=str, required=False, help='Postgres host')
+    parser.add_argument("--database", default='delirium_v3_0_0', type=str, required=False, help='Postgres database')
+    parser.add_argument("--output", type=str, required=True, help='Where should we put the CSV results?')
 
+    args = parser.parse_args()
+
+    # args.commit = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('ascii')
+
+    print({k: v for k, v in vars(args).items()})
+
+    password = getpass.getpass(prompt='Database password: ', stream=None)
+    args.password = password
+
+    return args
+
+def write_config(config):
+    # save args to args_{date}.json
+    t = time.localtime()
+    date = time.strftime("%Y-%b-%d_%H-%M-%S", t)
+    print(date)
+
+    with open(os.path.join(config.output, f'args_{date}.json'), 'w') as fp:
+        fp.write(json.dumps({k: v for k, v in vars(config).items() if k != 'password'}, indent=4))
+
+def extract(config):
+    print('postgresql://{config.user}:{config.password}@{config.host}:{config.port}/{config.database}')
+    engine = sqlalchemy.create_engine(f'postgresql://{config.user}:{config.password}@{config.host}:{config.port}/{config.database}')
+
+    # extract basic demographics and length of stay information from ip_administrative
     query = """select distinct
         i.patient_id_hashed as patient_id,
         i.genc_id,
@@ -20,8 +51,13 @@ def main(args):
         CASE when i.gender = 'F' THEN 1 ELSE 0 END AS sex,
         i.age,
         LOWER(i.country) as country,
-	i.discharge_date_time, 
-	i.admit_date_time,
+	    i.discharge_date_time, 
+	    i.admit_date_time,
+	    DATE_PART('year', i.admit_date_time) as year, 
+	    EXTRACT (
+          DAY
+           FROM(i.discharge_date_time - i.admit_date_time)) 
+           as stay_length,
         i.institution_from,
         i.institution_from_type,
         i.discharge_disposition::integer,
@@ -36,35 +72,32 @@ def main(args):
     print(data.head())
     print(data.columns.tolist())
 
-if __name__=="__main__":
-    import argparse
-    
-    # read and write args
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument("--user", default='koshkinam', type=str, required=False, help='Postgres user')
-    #parser.add_argument("--password", default=os.environ['PGPASSWORD'], type=str, required=True, help='Postgres password')
-    parser.add_argument("--port", default=5432, type=int, help='Postgres port')
-    parser.add_argument("--host", default='db.gemini-hpc.ca', type=str, required=False, help='Postgres host')
-    parser.add_argument("--database", default='delirium_v3_0_0' , type=str, required=False, help='Postgres database')
-    parser.add_argument("--output", type=str, required=True, help='Where should we put the CSV results?')
+    return data
 
-    args = parser.parse_args()
+def binary_legth_of_stay(l):
+    return 1 if l >= 7 else 0
 
-    #args.commit = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('ascii')
+def transform(data):
+    # convert length of stay feature
+    # 1 - more then 7 days, 0 - less
+    data["stay_length"].apply(binary_legth_of_stay)
+    return data
 
-    print({k:v for k,v in vars(args).items()})
-
-    password = getpass.getpass(prompt='Database password: ', stream=None) 
-    args.password = password
-    
-    # save args to args_{date}.json
+def save_to_disk(data, config, format=='csv'):
+    if (format != 'csv'):
+        print("Unsupported format {%s}".format(format))
+        exit
     t = time.localtime()
     date = time.strftime("%Y-%b-%d_%H-%M-%S", t)
-    print(date)
+    file_name = os.path.join(config.output, f'admin_data_{date}.csv')
+    data.to_csv(file_name)
 
-    with open(os.path.join(args.output, f'args_{date}.json'), 'w') as fp:
-        fp.write(json.dumps({k:v for k,v in vars(args).items() if k!='password'}, indent=4))
-    
-    main(args)
+
+if __name__=="__main__":
+    config = read_config()
+    write_config(config)
+
+    data = extract(config)
+    data = transform(data)
+    save_to_disk(data, config)
 
