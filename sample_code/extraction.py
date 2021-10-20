15f6995c3f0b75ee2abd944b1d1a4c16724192cb
mkoshkina <55889868+mkoshkina@users.noreply.github.com>
2021-10-20 11:13:19 -0400

    fix for diagnosis encodding

--- sample_code/extraction.py
+++ sample_code/extraction.py
@@ -10,6 +10,30 @@ import config as conf
 
 # constants
 HOSPITAL_ID = {'THPM':0, 'SBK':1, 'UHNTG':2, 'SMH':3, 'UHNTW':4, 'THPC':5, 'PMH':6, 'MSH':7}
+TRAJECTORIES = {
+        'Certain infectious and parasitic diseases': ('A00', 'B99'),
+        'Neoplasms': ('C00', 'D49'),
+        'Diseases of the blood and blood-forming organs and certain disorders involving the immune mechanism': ('D50','D89'),
+        'Endocrine, nutritional and metabolic diseases': ('E00', 'E89'),
+        'Mental, Behavioral and Neurodevelopmental disorders': ('F01', 'F99'),
+        'Diseases of the nervous system': ('G00', 'G99'),
+        'Diseases of the eye and adnexa': ('H00', 'H59'),
+        'Diseases of the ear and mastoid process': ('H60', 'H95'),
+        'Diseases of the circulatory system': ('I00', 'I99'),
+        'Diseases of the respiratory system': ('J00', 'J99'),
+        'Diseases of the digestive system': ('K00', 'K95'),
+        'Diseases of the skin and subcutaneous tissue': ('L00', 'L99'),
+        'Diseases of the musculoskeletal system and connective tissue': ('M00', 'M99'),
+        'Diseases of the genitourinary system': ('N00', 'N99'),
+        'Pregnancy, childbirth and the puerperium': ('O00', 'O99'),
+        'Certain conditions originating in the perinatal period': ('P00', 'P96'),
+        'Congenital malformations, deformations and chromosomal abnormalities': ('Q00','Q99'),
+        'Symptoms, signs and abnormal clinical and laboratory findings, not elsewhere classified': ('R00', 'R99'),
+        'Injury, poisoning and certain other consequences of external causes': ('S00', 'T88'),
+        'External causes of morbidity': ('V00', 'Y99'),
+        'COVID19': ('U07', 'U08'),
+        'Factors influencing health status and contact with health services': ('Z00', 'Z99')
+    }
 
 def extract(config):
     print('postgresql://{config.user}:{config.password}@{config.host}:{config.port}/{config.database}')
@@ -92,75 +116,51 @@ def extract(config):
 def binary_legth_of_stay(l):
     return 1 if l >= 7 else 0    #TODO: replace with parameter
 
-def transform_diagnosis(data):
-  trajectories = {
-        'Certain infectious and parasitic diseases': ('A00', 'B99'),
-        'Neoplasms': ('C00', 'D49'),
-        'Diseases of the blood and blood-forming organs and certain disorders involving the immune mechanism': ('D50','D89'),
-        'Endocrine, nutritional and metabolic diseases': ('E00', 'E89'),
-        'Mental, Behavioral and Neurodevelopmental disorders': ('F01', 'F99'),
-        'Diseases of the nervous system': ('G00', 'G99'),
-        'Diseases of the eye and adnexa': ('H00', 'H59'),
-        'Diseases of the ear and mastoid process': ('H60', 'H95'),
-        'Diseases of the circulatory system': ('I00', 'I99'),
-        'Diseases of the respiratory system': ('J00', 'J99'),
-        'Diseases of the digestive system': ('K00', 'K95'),
-        'Diseases of the skin and subcutaneous tissue': ('L00', 'L99'),
-        'Diseases of the musculoskeletal system and connective tissue': ('M00', 'M99'),
-        'Diseases of the genitourinary system': ('N00', 'N99'),
-        'Pregnancy, childbirth and the puerperium': ('O00', 'O99'),
-        'Certain conditions originating in the perinatal period': ('P00', 'P96'),
-        'Congenital malformations, deformations and chromosomal abnormalities': ('Q00','Q99'),
-        'Symptoms, signs and abnormal clinical and laboratory findings, not elsewhere classified': ('R00', 'R99'),
-        'Injury, poisoning and certain other consequences of external causes': ('S00', 'T88'),
-        'External causes of morbidity': ('V00', 'Y99'),
-        'COVID19': ('U07', 'U08'),
-        'Factors influencing health status and contact with health services': ('Z00', 'Z99')
-    }
-    
-    
-    def insert_decimal(string, index=2):
-        return string[:index] + '.' + string[index:]
-    
-    
-    
-    def get_category(code, trajectories=trajectories):
-        """
-        Usage:
-        df['ICD10'].apply(get_category, args=(trajectories,))
-        """
-        if code is None:
-            return np.nan
-        try:code=str(code)
-        except:return np.nan
-        for item, value in trajectories.items():
-            # check that code is greater than value_1
-            if (re.sub('[^a-zA-Z]', '', code).upper()>value[0][0].upper()):
-                # example, code is T and comparator is S
-                pass
-            elif (re.sub('[^a-zA-Z]', '', code).upper()==value[0][0].upper()) and (float(insert_decimal(re.sub('[^0-9]','', code), index=2))>=int(value[0][1:])):
-                # example S21 > s00
-                pass
-            else:continue
-                
-            # check that code is less than value_2
-            if (re.sub('[^a-zA-Z]', '', code).upper()<value[1][0].upper()):
-                # example, code is S and comparator is T
-    #             print(value[0], code, value[1])
-                return "_".join(value)
-            elif (re.sub('[^a-zA-Z]', '', code).upper()==value[1][0].upper()) and (int(float(insert_decimal(re.sub('[^0-9]','', code), index=2)))<=int(value[1][1:])):
-                # example S21 > s00
-    #             print(value[0], code, value[1])
-                return "_".join(value)
-            else:continue
-        raise Exception("Code cannot be converted: {}".format(code))  
-    
-    
-    # apply the categorical ICD10 filter and one hot encode:
-    data = pd.concat((data, pd.get_dummies(data.loc[:, 'mr_diagnosis'].apply(get_category, args=(trajectories,)), dummy_na=True, columns=trajectories.keys(), prefix='icd10')), axis=1)
-    
-   return data
+def insert_decimal(string, index=2):
+    return string[:index] + '.' + string[index:]
 
+def get_category(code, trajectories=TRAJECTORIES):
+   """
+    Usage:
+    df['ICD10'].apply(get_category, args=(trajectories,))
+    """
+    if code is None:
+        return np.nan
+    try:
+        code = str(code)
+    except:
+        return np.nan
+    for item, value in trajectories.items():
+        # check that code is greater than value_1
+        if (re.sub('[^a-zA-Z]', '', code).upper() > value[0][0].upper()):
+            # example, code is T and comparator is S
+            pass
+        elif (re.sub('[^a-zA-Z]', '', code).upper() == value[0][0].upper()) and (
+                float(insert_decimal(re.sub('[^0-9]', '', code), index=2)) >= int(value[0][1:])):
+            # example S21 > s00
+            pass
+        else:
+            continue
+
+        # check that code is less than value_2
+        if (re.sub('[^a-zA-Z]', '', code).upper() < value[1][0].upper()):
+            # example, code is S and comparator is T
+            #             print(value[0], code, value[1])
+            return "_".join(value)
+        elif (re.sub('[^a-zA-Z]', '', code).upper() == value[1][0].upper()) and (
+                int(float(insert_decimal(re.sub('[^0-9]', '', code), index=2))) <= int(value[1][1:])):
+            # example S21 > s00
+            #             print(value[0], code, value[1])
+            return "_".join(value)
+        else:
+            continue
+    raise Exception("Code cannot be converted: {}".format(code))
+
+def transform_diagnosis(data):
+    # apply the categorical ICD10 filter and one hot encode:
+    data = pd.concat((data, pd.get_dummies(data.loc[:, 'mr_diagnosis'].apply(get_category, args=(TRAJECTORIES,)), dummy_na=True, columns=TRAJECTORIES.keys(), prefix='icd10')), axis=1)
+    
+    return data
 
 def transform(data):
     # convert length of stay feature
