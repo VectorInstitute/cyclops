d29035c5c86010357d41e12047819aa5f7e35141
mkoshkina <55889868+mkoshkina@users.noreply.github.com>
2021-10-20 10:04:31 -0400

    update database select to include er and diagnosis

--- sample_code/extraction.py
+++ sample_code/extraction.py
@@ -19,21 +19,39 @@ def extract(config):
         i.hospital_id,
         CASE when i.gender = 'F' THEN 1 ELSE 0 END AS sex,
         i.age,
-        LOWER(i.country) as country,
+        CASE when i.discharge_disposition = 7 THEN 1 ELSE 0 END AS mort_hosp,
 	    i.discharge_date_time, 
 	    i.admit_date_time,
+	    f.diagnosis_code as mr_diagnosis,
 	    DATE_PART('year', i.admit_date_time) as year, 
-	    EXTRACT (
-          DAY
-           FROM(i.discharge_date_time - i.admit_date_time)) 
-           as stay_length,
-        i.institution_from,
-        i.institution_from_type,
-        i.discharge_disposition::integer,
-        i.institution_to,
-        i.institution_to_type,
-	i.province_territory_issuing_health_card_number as insurance
-      FROM ip_administrative i
+        (extract(epoch from i.discharge_date_time)::FLOAT - extract(epoch from i.admit_date_time)::float)/(24*60*60) as los,
+        CASE when NULLIF(REPLACE(REPLACE(i.readmission, 'Yes', '9'), 'No', '5'), '')::numeric::integer = 2 or  NULLIF(REPLACE(REPLACE(i.readmission, 'Yes', '9'), 'No', '5'), '')::numeric::integer = 4  THEN 1 ELSE 0 END AS readmission_7,
+        CASE when NULLIF(REPLACE(REPLACE(i.readmission, 'Yes', '9'), 'No', '5'), '')::numeric::integer = 2 or  NULLIF(REPLACE(REPLACE(i.readmission, 'Yes', '9'), 'No', '5'), '')::numeric::integer = 3 or  NULLIF(REPLACE(REPLACE(i.readmission, 'Yes', '9'), 'No', '5'), '')::numeric::integer = 4  THEN 1 ELSE 0 END AS readmission_28,
+        CASE when g.pal =1 THEN 1 ELSE 0 END AS palliative,
+        e.los_er,
+        e.admit_via_ambulance,
+        e.triage_date_time as er_admit_date_time,
+        e.left_er_date_time as er_discharge_date_time,
+        e.triage_level
+      FROM ip_administrative i,
+          LEFT OUTER JOIN (SELECT d.genc_id, d.diagnosis_code
+                  FROM diagnosis d
+                  WHERE d.diagnosis_type='M' AND d.is_er_diagnosis='FALSE') f
+                  ON i.genc_id=f.genc_id
+          LEFT OUTER JOIN (SELECT d.genc_id, 1 as pal
+                  FROM diagnosis d
+                  WHERE d.diagnosis_code = 'Z515') g
+                  ON i.genc_id=g.genc_id
+          LEFT OUTER JOIN (SELECT e.genc_id, 
+                                e.admit_via_ambulance,
+                                e.disposition_date_time,
+                                e.duration_er_stay_derived AS los_er,
+                                e.left_er_date_time,
+                                e.physician_initial_assessment_date_time,
+                                e.triate_date_time as triage_date_time,
+                                e.triage_level
+                  FROM er_administrative e) e
+                  ON i.genc_id=e.genc_id
       ORDER BY patient_id, genc_id
       LIMIT 10000"""
 
