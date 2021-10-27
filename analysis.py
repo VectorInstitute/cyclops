2c8f90cc7c0aae22592ac6c36f127928630fea06
Maria Koshkina <koshkinam@cpu1.gemini-hpc.ca>
2021-10-27 15:38:48 -0400

    update analysis config and add demo notebook

--- analysis.py
+++ analysis.py
@@ -20,6 +20,7 @@ def read_config(file = False):
     parser.add_argument("--type", type=str, default="dataset", help='Type of report to generate')
 
     # data-specific parameters
+    parser.add('--input', type=str, default=None, required=False, help='Data file to read from instead of database')
     parser.add('--slice', default='year', type=str, required=False,
            help='What column to use to slice data for analysis?')
     parser.add('--data_ref', default=[], type=int, action='append', required=False,
@@ -57,6 +58,10 @@ def analyze_dataset_drift(data, config):
     column_mapping = {}
     column_mapping['numerical_features'] = config.numerical_features
     column_mapping['categorical_features'] = config.categorical_features
+    if config.target_num:
+        column_mapping['numerical_features'] += [config.target]
+    else:
+       column_mapping['categorical_features'] += [config.target]
     analysis_columns = column_mapping['numerical_features'] + column_mapping['categorical_features']
 
     # prepare data - select only numeric and categorical features
@@ -68,12 +73,12 @@ def analyze_dataset_drift(data, config):
 
     eval_data = data.loc[data[config.slice].isin(eval_slices), analysis_columns]
     eval_data = eval_data.dropna()
-    drift = eval_drift(reference_data, eval_data, column_mapping, html=True)
+    drift = eval_drift(reference_data, eval_data, column_mapping,config,  html=True)
 
     return drift  
 
 #evaluate data drift with Evidently Profile
-def eval_drift(reference, production, column_mapping, html=False):
+def eval_drift(reference, production, column_mapping, config, html=False):
     column_mapping['drift_conf_level'] = 0.95
     column_mapping['drift_features_share'] = 0.5
     data_drift_profile = Profile(sections=[DataDriftProfileSection])
@@ -104,13 +109,16 @@ def analyze_model_drift(reference, test, config):
     perfomance_dashboard.calculate(reference, test, column_mapping=column_mapping)
 
     perfomance_dashboard.save(get_report_filename(config))
-    
-if __name__ == "__main__":
-    config = read_config()
-    if args.type == "dataset":
+
+def main(config):
+    if config.type == "dataset":
         data = pd.read_csv(config.input)
         analyze_dataset_drift(data, config)
     else:
         reference = pd.read_csv(config.reference)
         test = pd.read_csv(config.test)
         analyze_model_drift(reference, test, config)
+
+if __name__ == "__main__":
+    config = read_config()
+    main(config)
