a07180948c6f348c59676006483d3b9f1477bb98
Amrit Krishnan <53-krishnanam@users.noreply.code.gemini-hpc.ca>
2022-01-31 08:59:50 -0500

    Oops, leaked my creds by mistake lol

--- cyclops/orm.py
+++ cyclops/orm.py
@@ -1,17 +1,37 @@
 """Figure out ORMs."""
 
-
+import logging
 from collections import defaultdict
 
 from sqlalchemy import select, case
-from sqlalchemy import DateTime, Float, Integer, String
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 
+import config
+from cyclops.utils.log import setup_logging, LOG_FILE_PATH
+
+
+# Logging.
+LOGGER = logging.getLogger(__name__)
+setup_logging(log_path=LOG_FILE_PATH, print_level="INFO", logger=LOGGER)
+
+
+def _get_db_url(dbms, user, pwd, host, port, db):
+    return f"{dbms}://{user}:{pwd}@{host}:{port}/{db}"
+
+
+cfg = config.read_config("../configs/default/*.yaml")
 
 Base = automap_base()
-CONN_STR = "postgresql://krishnanam:Totem110#@db.gemini-hpc.ca:5432/delirium_v3_0_0"
+CONN_STR = _get_db_url(
+    cfg.dbms,
+    cfg.user,
+    cfg.password,
+    cfg.host,
+    cfg.port,
+    cfg.database,
+)
 engine = create_engine(CONN_STR, echo=True)
 session = Session(engine, future=True)
 
@@ -19,7 +39,7 @@ Base.prepare(engine, reflect=True)
 
 table_names = ["ip_administrative", "er_administrative", "diagnosis"]
 
-tables = defaultdict(None)
+tables = defaultdict(None)  # type: ignore
 for table_name in table_names:
     tables[table_name] = getattr(Base.classes, table_name)
 
@@ -31,10 +51,11 @@ statement = select(
     getattr(tables["ip_administrative"], "hospital_id"),
     case((getattr(tables["ip_administrative"], "gender") == "F", 1), else_=0),
     getattr(tables["ip_administrative"], "age"),
-    case((getattr(tables["ip_administrative"], "discharge_disposition") == 7, 1), else_=0),
+    case(
+        (getattr(tables["ip_administrative"], "discharge_disposition") == 7, 1), else_=0
+    ),
 )
 
 # list of tuples
 result = session.execute(statement).first()
-
 print(len(result), result)
