e838c664cce20a26faecf2f4c9badc90ce29489c
Amrit <amritk@vectorinstitute.ai>
2022-02-28 15:57:36 -0500

    Add images and updated diagram fn

--- draw_sys_design.py
+++ draw_sys_design.py
@@ -1,9 +1,13 @@
 """Draw system level evaluation framework architecture diagram."""
 
 from os.path import join
-from diagrams import Diagram, Cluster
+from diagrams import Diagram, Cluster, Edge
 from diagrams.custom import Custom
 from diagrams.programming.language import Python
+from diagrams.generic.storage import Storage
+from diagrams.programming.flowchart import Database
+from diagrams.generic.database import SQL
+from diagrams.onprem.queue import Kafka
 
 
 ASSETS_DIR = "./assets"
@@ -68,6 +72,42 @@ def draw_implementation_extended():
         prefect >> prefect_dashboard
 
 
+def draw_dr_cyclops():
+    """Draw design of components of dr_cyclops."""
+    with Diagram("dr_cyclops", filename="dr_cyclops", show=False):
+        with Cluster("hospital site A"):
+            hospA = Custom("", join(ASSETS_DIR, "hospAB.png"))
+            dbA = Custom("", join(ASSETS_DIR, "dbA.png"))
+        with Cluster("hospital site B"):
+            hospB = Custom("", join(ASSETS_DIR, "hospAB.png"))
+            dbB = Custom("", join(ASSETS_DIR, "dbB.png"))
+        with Cluster("hospital site Z"):
+            hospZ = Custom("", join(ASSETS_DIR, "hospZ.png"))
+            dbZ = Custom("", join(ASSETS_DIR, "dbZ.png"))
+        storage = Database("Databases (GEMINI/MIMIC)")
+        stream = Custom("", join(ASSETS_DIR, "stream.png"))
+        with Cluster("dr_cyclops data layer"):
+            extract = SQL("extract")
+            process = Custom("processing", join(ASSETS_DIR, "process.png"))
+            feast = Custom("feature store", join(ASSETS_DIR, "feast_logo.png"))
+
+        with Cluster("dr_cyclops model layer"):
+            model_tr = Custom("model training", join(ASSETS_DIR, "model.png"))
+            model_ev = Custom("model evaluation", join(ASSETS_DIR, "eval.png"))
+        with Cluster("dr_cyclops drift-detection layer"):
+            drift = Custom("drift detection", join(ASSETS_DIR, "drift.png"))
+
+        dbA >> storage
+        dbB >> storage
+        dbZ >> stream >> process
+        hospA - Edge(color="brown", style="dotted") - hospB
+        storage >> extract >> process >> feast
+        feast >> model_tr
+        feast >> model_ev
+        feast >> drift
+
+
 if __name__ == "__main__":
-    draw_implementation_on_gemini()
+    draw_data_layer()
+    # draw_implementation_on_gemini()
     # draw_implementation_extended()
