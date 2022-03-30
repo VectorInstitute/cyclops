c92c68403451da0d21020b9b23cccc77f6faeba0
Amrit <amritk@vectorinstitute.ai>
2022-03-30 01:04:12 -0400

    Refactor config parameters, add tests

---
+++ tests/cyclops/processors/test_config.py
@@ -0,0 +1,71 @@
+"""Test configuration module."""
+
+import os
+import shutil
+import unittest
+import json
+import yaml
+
+import config
+from codebase_ops import PROJECT_ROOT
+
+
+def _save_to_yaml(to_save: dict, file_path: str) -> None:
+    with open(file_path, "w", encoding="utf8") as file_handle:
+        yaml.safe_dump(
+            to_save,
+            file_handle,
+        )
+
+
+class TestConfig(unittest.TestCase):
+    """Test cases for config."""
+
+    def setUp(self):
+        """Set up dummy config and output directories."""
+        self.dummy_config_dir = "/tmp/config"
+        os.makedirs(self.dummy_config_dir, exist_ok=True)
+        dummy_cfg = {"database": "test", "years": [2020, 2019]}
+        _save_to_yaml(dummy_cfg, os.path.join(self.dummy_config_dir, "data.yaml"))
+        _save_to_yaml({}, os.path.join(self.dummy_config_dir, "model.yaml"))
+        _save_to_yaml({}, os.path.join(self.dummy_config_dir, "drift.yaml"))
+        _save_to_yaml({}, os.path.join(self.dummy_config_dir, "workflow.yaml"))
+
+    def tearDown(self):
+        """Remove temp files after running tests."""
+        shutil.rmtree(self.dummy_config_dir)
+
+    def test_read_config(self):
+        """Test reading configuration."""
+        os.environ["PGPASSWORD"] = "test"
+        cfg = config.read_config()
+        assert cfg.password == "test"
+
+        cfg = config.read_config(os.path.join(PROJECT_ROOT, "configs/mimic"))
+        assert cfg.database == "mimiciv"
+
+        cfg = config.read_config(os.path.join(PROJECT_ROOT, "configs/mimic/*.yaml"))
+        assert cfg.database == "mimiciv"
+
+        cfg = config.read_config(self.dummy_config_dir)
+        assert cfg.database == "test"
+        assert cfg.years == ["2020", "2019"]
+
+    def test_config_to_dict(self):
+        """Test config_to_dict fn."""
+        os.environ["PGPASSWORD"] = "test"
+        cfg = config.read_config(self.dummy_config_dir)
+        cfg_dict = config.config_to_dict(cfg)
+        assert isinstance(cfg_dict, dict)
+        assert "password" not in cfg_dict
+
+    def test_write_config(self):
+        """Test write_config fn."""
+        os.environ["PGPASSWORD"] = "test"
+        cfg = config.read_config(self.dummy_config_dir)
+        file_path = config.write_config(cfg)
+        assert os.path.isfile(file_path)
+        with open(file_path, "r", encoding="utf8") as file_handle:
+            cfg_written = json.load(file_handle)
+        assert cfg_written["database"] == "test"
+        assert cfg_written["years"] == ["2020", "2019"]
