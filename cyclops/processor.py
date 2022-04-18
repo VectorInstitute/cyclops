ef9ca0b43b189d398682181e259242d699e2717d
Amrit Krishnan <krishnanam@cpu1.gemini-hpc.ca>
2022-04-18 09:32:57 -0400

    WIP

--- cyclops/processor.py
+++ cyclops/processor.py
@@ -5,11 +5,11 @@ from typing import Union
 
 import pandas as pd
 
-from cyclops.processors.admin import AdminProcessor
 from cyclops.processors.column_names import (
     ADMIT_TIMESTAMP,
     AGE,
     ENCOUNTER_ID,
+    DIAGNOSIS_CODE,
     LAB_TEST_NAME,
     LAB_TEST_RESULT_UNIT,
     LAB_TEST_RESULT_VALUE,
@@ -20,18 +20,10 @@ from cyclops.processors.column_names import (
     VITAL_MEASUREMENT_TIMESTAMP,
     VITAL_MEASUREMENT_VALUE,
 )
+from cyclops.processors.utils import check_must_have_columns, gather_columns
+from cyclops.processors.aggregate import Aggregator, gather_static_features
+from cyclops.processors.diagnoses import group_diagnosis_codes_to_trajectories
 from cyclops.processors.feature_handler import FeatureHandler
-from cyclops.processors.labs import LabsProcessor
-from cyclops.processors.vitals import VitalsProcessor
-
-
-@dataclass
-class Aggregator:
-    """Aggregator options."""
-
-    strategy: str = "static"
-    range_: int = 168
-    window: int = 24
 
 
 @dataclass
@@ -42,7 +34,8 @@ class Imputer:
 
 
 def featurize(
-    data: Union[list, pd.DataFrame],
+    static_data: Union[list, pd.DataFrame],
+    temporal_data: Union[list, pd.DataFrame],
     aggregator: Aggregator = Aggregator(),  # pylint:disable=unused-argument
     imputer: Imputer = Imputer(),  # pylint:disable=unused-argument
 ) -> FeatureHandler:
@@ -62,38 +55,18 @@ def featurize(
         Feature handler object, which is a container for processed features.
 
     """
-    admin_processor = AdminProcessor(data[0], [ENCOUNTER_ID, AGE, SEX])
-    admin_features = admin_processor.process()
-
-    labs_processor = LabsProcessor(
-        data[0],
-        [
-            ENCOUNTER_ID,
-            ADMIT_TIMESTAMP,
-            LAB_TEST_NAME,
-            LAB_TEST_TIMESTAMP,
-            LAB_TEST_RESULT_VALUE,
-            LAB_TEST_RESULT_UNIT,
-            REFERENCE_RANGE,
-        ],
-    )
-    labs_features = labs_processor.process()
-    vitals_processor = VitalsProcessor(
-        data[1],
-        [
-            ENCOUNTER_ID,
-            ADMIT_TIMESTAMP,
-            VITAL_MEASUREMENT_NAME,
-            VITAL_MEASUREMENT_VALUE,
-            VITAL_MEASUREMENT_TIMESTAMP,
-            REFERENCE_RANGE,
-        ],
-    )
-    vitals_features = vitals_processor.process()
-
     feature_handler = FeatureHandler()
-    feature_handler.add_features(admin_features)
-    feature_handler.add_features(labs_features)
-    feature_handler.add_features(vitals_features)
+    
+    if isinstance(static_data, pd.DataFrame):
+        static_data = [static_data]
+        
+    for dataframe in static_data:
+        if check_must_have_columns(dataframe, [ENCOUNTER_ID, DIAGNOSIS_CODE]):
+            diagnoses_data = gather_columns(dataframe, [ENCOUNTER_ID, DIAGNOSIS_CODE])
+            diagnoses_features = group_diagnosis_codes_to_trajectories(diagnoses_data)
+            feature_handler.add_features(diagnoses_features)
+            dataframe.drop(DIAGNOSIS_CODE, axis=1, inplace=True)
+        static_features = gather_static_features(dataframe)
+        feature_handler.add_features(static_features)
 
     return feature_handler
--- cyclops/processors/utils.py
+++ cyclops/processors/utils.py
@@ -1,130 +1,17 @@
 """Utility functions for processor API."""
 
 import logging
-from typing import Union
 
 import pandas as pd
 
 from codebase_ops import get_log_file_path
-from cyclops.processor import Aggregator
-from cyclops.processors.column_names import ADMIT_TIMESTAMP, ENCOUNTER_ID
+from cyclops.processors.column_names import ENCOUNTER_ID
 from cyclops.utils.log import setup_logging
-from cyclops.utils.profile import time_function
+
 
 # Logging.
 LOGGER = logging.getLogger(__name__)
 setup_logging(log_path=get_log_file_path(), print_level="INFO", logger=LOGGER)
-
-
-def filter_within_admission_window(
-    data: pd.DataFrame, sample_ts_col_name, aggregation_window: int = 24
-) -> pd.DataFrame:
-    """Filter data based on single time window value.
-
-    For e.g. if window is 24 hrs, then all data 24 hrs
-    before time of admission and after 24 hrs of admission are considered.
-
-    Parameters
-    ----------
-    data: pandas.DataFrame
-        Data before filtering.
-    sample_ts_col_name: str
-        Name of column corresponding to the sample timestamp.
-    aggregation_window: int, optional
-        Window (no. of hrs) before and after admission to consider.
-
-    Returns
-    -------
-    pandas.DataFrame
-        Filtered data frame, with aggregates collected within window.
-
-    """
-    data_filtered = data.copy()
-    sample_time = data_filtered[sample_ts_col_name]
-    admit_time = data_filtered[ADMIT_TIMESTAMP]
-    window_condition = abs((sample_time - admit_time) / pd.Timedelta(hours=1))
-    data_filtered = data_filtered.loc[window_condition <= aggregation_window]
-    return data_filtered
-
-
-@time_function
-def gather_event_features(
-    data, groupby_col, event_col, aggregator: Aggregator
-) -> Union[pd.DataFrame, pd.MultiIndex]:
-    """Gather events from encounters into statistical (static) or time-series features.
-
-    Parameters
-    ----------
-    data: pandas.DataFrame
-        Input data.
-    groupby_col: str
-        Name of column to use to group data, e.g. column with lab test names
-        can be used to group lab test data.
-    event_col: str
-        Name of column with event info to use for creating features. e.g. lab
-        test result values, or vital measurement value.
-    aggregator: cyclops.processor.Aggregator
-        Aggregation options.
-
-    Returns
-    -------
-    pandas.DataFrame or pd.MultiIndex:
-        Processed event features.
-
-    """
-    log_counts_step(data, "Gathering event features...", columns=True)
-    event_names = list(data[groupby_col].unique())
-    encounters = list(data[ENCOUNTER_ID].unique())
-    features = pd.DataFrame(index=encounters, columns=event_names)
-
-    grouped_events = data.groupby([ENCOUNTER_ID, groupby_col])
-    for (encounter_id, event_name), events in grouped_events:
-        features.loc[encounter_id, event_name] = events[event_col].mean()
-
-    return features
-
-
-@time_function
-def gather_static_features(data) -> pd.DataFrame:
-    """Gathers encounter specific static features.
-
-    Patient statics gathered into features. This function groups patient static
-    information, checks to see if there is a unique static value for that given
-    encounter, and creates a feature column with that unique value.
-
-    Parameters
-    ----------
-    data: pandas.DataFrame
-        Input DataFrame.
-
-    Returns
-    -------
-    pandas.DataFrame:
-        Processed static features.
-
-    Raises
-    ------
-    AssertionError
-        Asserts to ensure that each static features have a single unique value
-        for a patient for the encounter.
-
-    """
-    log_counts_step(data, "Gathering static features...", columns=True)
-    encounters = list(data[ENCOUNTER_ID].unique())
-    col_names = list(data.columns)
-    features = pd.DataFrame(index=encounters, columns=col_names)
-
-    grouped = data.groupby([ENCOUNTER_ID])
-    for encounter_id, statics in grouped:
-        for col_name in col_names:
-            if statics[col_name].nunique() != 1:
-                raise AssertionError(
-                    """Duplicate values encountered in patient statics,
-                    in {col_name} column!"""
-                )
-            features.loc[encounter_id, col_name] = statics[col_name].unique()[0]
-
-    return features
 
 
 def check_must_have_columns(data: pd.DataFrame, must_have_columns: list) -> bool:
