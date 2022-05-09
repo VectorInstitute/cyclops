27271fade98572955fbfba00a5b4f959c87dfdfa
Amrit K <amritk@vectorinstitute.ai>
2022-05-09 11:50:22 -0400

    Improve db connection, print error msgs and not raise errors

--- tests/cyclops/test_orm.py
+++ tests/cyclops/test_orm.py
@@ -1,16 +1,12 @@
 """Test ORM implementation."""
 
-import socket
-
-import pytest
-
 from cyclops.config import read_config
-from cyclops.constants import GEMINI
+from cyclops.constants import GEMINI, MIMIC
 from cyclops.orm import Database
 
 
 def test_db_instantiation():
     """Test instantiation of DB that implements ORM."""
-    # Attempts to connect to GEMINI and runs into error.
-    with pytest.raises(socket.gaierror):
-        _ = Database(read_config(GEMINI))
+    # Attempts to connect to GEMINI and MIMIC but just prints logger warning.
+    _ = Database(read_config(GEMINI))
+    _ = Database(read_config(MIMIC))
