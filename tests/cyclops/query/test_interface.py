49300faec4a475bd3fe1d1f34aba1267bf6dda9a
Amrit K <amritk@vectorinstitute.ai>
2022-05-18 16:41:48 -0400

    Improve test for interface, add same for QueryInterfaceProcessed

--- tests/cyclops/query/test_interface.py
+++ tests/cyclops/query/test_interface.py
@@ -7,17 +7,24 @@ from unittest.mock import patch
 import pandas as pd
 import pytest
 
-from cyclops.query.interface import QueryInterface, save_queried_dataframe
+from cyclops.query.interface import (
+    QueryInterface,
+    QueryInterfaceProcessed,
+    save_queried_dataframe,
+)
 
 
-def test_save_queried_dataframe():
+@pytest.fixture
+def test_data():
+    """Dummy dataframe for testing."""
+    return pd.DataFrame([[1, "a", 1], [5.1, "b", 0]], columns=["col1", "col2", "col3"])
+
+
+def test_save_queried_dataframe(test_data):  # pylint: disable=redefined-outer-name
     """Test save fn."""
-    dummy_data = pd.DataFrame(
-        [[1, "a", 1], [5.1, "b", 0]], columns=["col1", "col2", "col3"]
-    )
-    save_queried_dataframe(dummy_data, "test_save", "test_features")
+    save_queried_dataframe(test_data, "test_save", "test_features")
     loaded_data = pd.read_parquet(os.path.join("test_save", "test_features.gzip"))
-    assert loaded_data.equals(dummy_data)
+    assert loaded_data.equals(test_data)
     shutil.rmtree("test_save")
     # Does nothing, just prints log warning!
     save_queried_dataframe(None, "test_save", "test_features")
@@ -25,8 +32,41 @@ def test_save_queried_dataframe():
 
 @patch("cyclops.orm.Database")
 @patch("sqlalchemy.sql.selectable.Subquery")
-def test_query_interface(database, query):
+def test_query_interface(
+    database, query, test_data  # pylint: disable=redefined-outer-name
+):
     """Test QueryInterface."""
     query_interface = QueryInterface(database, query)
     with pytest.raises(ValueError):
         _ = query_interface.run(filter_columns=["a"], filter_recognised=True)
+
+    query_interface.data = test_data
+    query_interface.save("test_save", "test_features")
+    loaded_data = pd.read_parquet(os.path.join("test_save", "test_features.gzip"))
+    assert loaded_data.equals(test_data)
+    shutil.rmtree("test_save")
+
+    # Doesn't test the actual filtering of the query attributes, just the call.
+    _ = query_interface.run(filter_columns=["a"])
+    _ = query_interface.run(filter_recognised=True)
+
+
+@patch("cyclops.orm.Database")
+@patch("sqlalchemy.sql.selectable.Subquery")
+def test_query_interface_processed(
+    database, query, test_data  # pylint: disable=redefined-outer-name
+):
+    """Test QueryInterface."""
+    query_interface = QueryInterfaceProcessed(database, query)
+    with pytest.raises(ValueError):
+        _ = query_interface.run(filter_columns=["a"], filter_recognised=True)
+
+    query_interface.data = test_data
+    query_interface.save("test_save", "test_features")
+    loaded_data = pd.read_parquet(os.path.join("test_save", "test_features.gzip"))
+    assert loaded_data.equals(test_data)
+    shutil.rmtree("test_save")
+
+    # Doesn't test the actual filtering of the query attributes, just the call.
+    _ = query_interface.run(filter_columns=["a"])
+    _ = query_interface.run(filter_recognised=True)
