073c8b36123c890fd001dc8748477079264f8aac
Valli Subasri <subasriv@cpu01.gemini-hpc.ca>
2022-07-04 11:12:22 -0400

    add more plotting utils for rolling window

--- drift_detection/drift_detector/rolling_window.py
+++ drift_detection/drift_detector/rolling_window.py
@@ -1,27 +1,32 @@
 import numpy as np
+import os
+import random
+import sys
+import pandas as pd
 
-def dynamic_rolling_window(x_val, stride, window, timesteps, series, threshold):
+#####################################################
+## rolling window - not cumulatively including data and no adjustment made when drift occurs
+##################################################### 
+def rolling_window(shift_detector, sample, stat_window, lookup_window, stride, num_timesteps, series, threshold,custom_ref=None):
 
     p_vals = np.asarray([])
     dist_vals = np.asarray([])
-
-    run_length = int(window)
-    i = window
-
-#    for single_date in daterange(start_date, end_date):
-#        print(single_date.strftime("%Y-%m-%d"))
-    #(single_date+timedelta(days=window)).strftime("%Y-%m-%d")
-    
-    
-    while i+stride+window <= len(series):
+    i = 0 
+    if custom_ref is not None:
+        prev = reshape_inputs(custom_ref, num_timesteps)
+        
+    while i+stat_window+lookup_window < len(series):
         feat_index = 0
         
-        prev = pd.concat(series[max(int(i)-run_length,0):int(i)])
-        prev = prev[~prev.index.duplicated(keep='first')]
-        prev = reshape_inputs(prev, num_timesteps)
-        next = pd.concat(series[max(int(i)+window,0):int(i)+stride+window])
+        if custom_ref is None:
+            prev = pd.concat(series[i:i+stat_window])
+            prev = prev[~prev.index.duplicated(keep='first')]
+            prev = reshape_inputs(prev, num_timesteps)
+            
+        next = pd.concat(series[i+lookup_window:i+lookup_window+stat_window])
         next = next[~next.index.duplicated(keep='first')]
         next = reshape_inputs(next, num_timesteps)
+        
         if next.shape[0]<=2 or prev.shape[0]<=2:
             break
             
@@ -29,25 +34,22 @@ def dynamic_rolling_window(x_val, stride, window, timesteps, series, threshold):
         cd = MMDDrift(prev, backend='pytorch', p_val=.05)
         preds = cd.predict(next, return_p_val=True, return_distance=True)
         p_val = preds['data']['p_val']
-        print(max(int(i)-run_length,0),"-", int(i),"-->",max(int(i)+window,0),"-",int(i)+stride+window,"\tP-Value: ",p_val)
-        dist_val = preds['data']['distance']
-
-        #####################################
-        if p_val >= threshold:
-            dist_vals = np.concatenate((dist_vals, np.repeat(dist_val, 1)))
-            dist_vals = np.concatenate((dist_vals, np.repeat(0, stride-1)))
-            p_vals = np.concatenate((p_vals, np.repeat(p_val, 1)))
-            p_vals = np.concatenate((p_vals, np.repeat(0, stride-1)))
-            i += stride
-            run_length += stride
-        else:
-            dist_vals = np.concatenate((dist_vals, np.repeat(dist_val, 1)))
-            p_vals = np.concatenate((p_vals, np.repeat(p_val, 1)))
-            i+=1
-            run_length = stride
         
-        if i == 500:
-            break
+#        (p_val, dist, val_acc, te_acc) = shift_detector.detect_data_shift(X_tr, 
+#                                                                          y_tr, 
+#                                                                          X_val, 
+#                                                                          y_val, 
+#                                                                          X_t[:sample, :], 
+#                                                                          y_t[:sample], 
+#                                                                          orig_dims
+#        )
+            
+        if p_val < threshold:
+            print("P-value below threshold.")
+            print(i,"-", i+stat_window,"-->",i+lookup_window,"-",i+stat_window+lookup_window,"\tP-Value: ",p_val)
+        dist_val = preds['data']['distance']
+        dist_vals = np.concatenate((dist_vals, np.repeat(dist_val, 1)))
+        p_vals = np.concatenate((p_vals, np.repeat(p_val, 1)))
+        i += stride
             
     return dist_vals, p_vals
-
