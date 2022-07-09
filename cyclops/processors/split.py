f2a745239c0a46bba00854693e0667a6d04615ba
Kaden McKeen <mckeen.kaden.school@hotmail.com>
2022-07-09 08:04:45 -0400

    Pre-commit fixes on split.py and test_split.py.

--- cyclops/processors/split.py
+++ cyclops/processors/split.py
@@ -1,39 +1,55 @@
 """datasetset splits."""
 
+from typing import List, Optional, Tuple, Union
+
 import numpy as np
-from typing import Union, List, Tuple, Optional
-from cyclops.utils.common import to_list, to_list_optional
+
+from cyclops.utils.common import to_list
+
 
 def fractions_to_split(
-    fractions: Union[float, List, Tuple],
+    fractions: Union[float, List[float]],
     data_len: int,
 ) -> np.ndarray:
+    """Turn a number of fractions into a dividing list of lengths at which to split.
+
+    Parameters
+    ----------
+    fractions: float or list of float
+        Fraction(s) of samples between 0 and 1 to use for each split.
+    data_len: int
+        The total number of samples in the data being split.
+
+    """
     if isinstance(fractions, float):
-        fractions = to_list(fractions)
-        
-    if isinstance(fractions, list) or isinstance(fractions, tuple):
-        is_float = [isinstance(elem, float) for elem in fractions]
-        if not all(is_float):
-            raise ValueError("fractions must be floats.")
-        
-        invalid = [frac <= 0 or frac >= 1 for frac in fractions]
-        if any(invalid):
-            raise ValueError("fractions must be between 0 and 1.")
-        
-        if sum(fractions) != 1:
-            if sum(fractions) < 1:
-                fractions.append(1 - sum(fractions))
-            else:
-                raise ValueError("fractions must sum to 1.")
+        frac_list = to_list(fractions)
+    elif isinstance(fractions, list):
+        frac_list = fractions
     else:
         raise ValueError("fractions must be a float or a list of floats.")
-    
-    for i in range(1, len(fractions)):
-        fractions[i] += fractions[i - 1]
-    
-    assert fractions[-1] == 1
-    
-    return np.round(np.array(fractions[:-1])*data_len).astype(int)
+
+    # Element checking
+    is_float = [isinstance(elem, float) for elem in frac_list]
+    if not all(is_float):
+        raise ValueError("fractions must be floats.")
+
+    invalid = [frac <= 0 or frac >= 1 for frac in frac_list]
+    if any(invalid):
+        raise ValueError("fractions must be between 0 and 1.")
+
+    if sum(frac_list) != 1:
+        if sum(frac_list) < 1:
+            frac_list.append(1 - sum(frac_list))
+        else:
+            raise ValueError("fractions must sum to 1.")
+
+    # Turn into dividing list of lengths to split and return
+    for i in range(1, len(frac_list)):
+        frac_list[i] += frac_list[i - 1]
+
+    assert frac_list[-1] == 1
+
+    return np.round(np.array(frac_list[:-1]) * data_len).astype(int)
 
 
 def split_idx(
@@ -49,8 +65,7 @@ def split_idx(
     data: numpy.ndarray
         Data.
     fractions: list, optional
-        Fraction of samples to use for train, test sets. If an int,
-        split into 2. If a list, split into .........................................................
+        Fraction(s) of samples between 0 and 1 to use for each split.
     randomize: bool, default = True
         Whether to randomize the data in the splits.
     seed: int, optional
@@ -64,12 +79,12 @@ def split_idx(
     """
     split = fractions_to_split(fractions, data_len)
     idx = np.arange(data_len)
-    
+
     if seed is not None:
         np.random.seed(seed)
     if randomize:
         np.random.shuffle(idx)
-    
+
     return tuple(np.split(idx, split))
 
 
@@ -79,44 +94,57 @@ def split_data(
     axes: Optional[Union[int, List[int]]] = None,
     randomize: bool = True,
     seed: int = None,
-) -> Union[np.ndarray, Tuple]:
-    """
+) -> Tuple:
+    """Split a dataset into a number of datasets.
+
+    Parameters
+    ----------
     datasets: np.ndarray or list of np.ndarray
         Datasets, or a dataset, to split.
     axes: int or list of int
         Axes, or axis, along which to split the data.
     fractions: float or list of float
         Fraction(s) of samples between 0 and 1 to use for each split.
+
+    Returns
+    -------
+    tuple
+        A tuple of splits if a single dataset is given. Otherwise, a tuple of
+        datasets of splits. All splits are also numpy.ndarray.
+
     """
     if isinstance(datasets, np.ndarray):
         datasets = [datasets]
-    axes = to_list_optional(axes)
-    
+
     if axes is None:
-        axes = [0]*len(datasets)
-    
-    sizes = [np.size(data, axes[i]) for i, data in enumerate(datasets)]
-    
+        axes_list = [0] * len(datasets)
+    else:
+        axes_list = to_list(axes)
+
+    sizes = [np.size(data, axes_list[i]) for i, data in enumerate(datasets)]
+
     # Make sure sizes along the specified axes are all the same
     if not sizes.count(sizes[0]) == len(sizes):
         raise ValueError("datasets must have the same sizes along the given axes.")
-    
+
     idx_splits = split_idx(fractions, sizes[0], randomize=randomize, seed=seed)
-    
-    splits = []
+
+    splits = []  # type: ignore
     # For each dataset
     for i, data in enumerate(datasets):
         splits.append([])
         # For each split
         for idx in idx_splits:
-            shape = [1]*len(data.shape)
-            shape[axes[i]] = len(idx)
+            # Reshape idx to have same number of dimensions as the data
+            shape = [1] * len(data.shape)
+            shape[axes_list[i]] = len(idx)
             idx = idx.reshape(shape)
-            print(data.shape)
-            print(idx.shape)
-            splits[-1].append(np.take_along_axis(data, idx, axis=axes[i]))
-    
+
+            # Sample new dataset split
+            splits[-1].append(np.take_along_axis(data, idx, axis=axes_list[i]))
+        splits[-1] = tuple(splits[-1])
+
     if len(splits) == 1:
         return splits[0]
-    
+
     return tuple(splits)
--- tests/cyclops/processors/test_split.py
+++ tests/cyclops/processors/test_split.py
@@ -1,15 +1,14 @@
 """Test dataset splits."""
 
 import numpy as np
-from cyclops.processors.split import (
-    fractions_to_split,
-    split_idx,
-    split_data,
-)
+
+from cyclops.processors.split import fractions_to_split, split_data, split_idx
+
 
 def test_fractions_to_split():
     """Test fractions_to_split function."""
     assert fractions_to_split(0.8, 100) == [80]
+
 
 def test_split_idx():
     """Test split_idx function."""
@@ -17,6 +16,7 @@ def test_split_idx():
     split2 = split_idx(0.2, 100, seed=3)
     assert np.array_equal(split1[0], split2[0])
     assert np.array_equal(split1[1], split2[1])
+
 
 def test_split_data():
     """Test split_data function."""
@@ -27,14 +27,15 @@ def test_split_data():
     train_labels, val_labels = splits[1]
     assert np.array_equal(train_data, train_labels)
     assert np.array_equal(val_data, val_labels)
-    
+
     labels = np.array([1])
-    
+
     try:
         split_data([data, labels], 0.8)
         raise ValueError(
-            ("An error should have been thrown since data/labels have a different",
-             "number of samples along the axis."
+            (
+                "An error should have been thrown since data/labels have a different",
+                "number of samples along the axis.",
             )
         )
     except ValueError:
