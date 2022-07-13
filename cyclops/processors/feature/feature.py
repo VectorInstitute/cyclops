Created vectorization functionality with normalization. Fixed GroupbyNormalizer indexing bug where normalizer hangs on groupby apply return. Continued worked on WangLab use case.

cyclops/processors/feature/feature.py
@@ -1,9 +1,11 @@
 """Feature processing."""
 
 import logging
-from typing import Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union, Tuple
 
+import numpy as np
 import pandas as pd
+import copy
 
 from codebase_ops import get_log_file_path
 from cyclops.plotter import plot_histogram, plot_temporal_features
@@ -22,11 +24,13 @@ from cyclops.processors.constants import (
     ORDINAL,
 )
 from cyclops.processors.feature.normalization import GroupbyNormalizer
+from cyclops.processors.feature.split import split_idx, intersect_datasets
 from cyclops.processors.feature.type_handling import (
     infer_types,
     normalize_data,
     to_types,
 )
+from cyclops.processors.feature.vectorize import Vectorized
 from cyclops.processors.util import has_columns, has_range_index, to_range_index
 from cyclops.utils.common import to_list, to_list_optional
 from cyclops.utils.file import save_dataframe
@@ -166,6 +170,7 @@ class Features:
         by: Optional[Union[str, List[str]]],  # pylint: disable=invalid-name
         targets: Optional[Union[str, List[str]]] = None,
         force_types: Optional[dict] = None,
+        normalizers: Optional[Dict[str, GroupbyNormalizer]] = None,
     ):
         """Init."""
         # Check data
@@ -180,40 +185,51 @@ class Features:
         # Force a range index
         data = to_range_index(data)
 
-        self.by = to_list_optional(by)  # pylint: disable=invalid-name
-
-        if self.by is not None:
-            has_columns(data, self.by, raise_error=True)
-
         feature_list = to_list(features)
         target_list = to_list_optional(targets, none_to_empty=True)
+        
+        if len(feature_list) == 0:
+            raise ValueError("Must specify at least one feature.")
 
         has_columns(data, feature_list, raise_error=True)
         has_columns(data, target_list, raise_error=True)
 
+        self.by = to_list_optional(by)  # pylint: disable=invalid-name
+
+        if self.by is not None:
+            has_columns(data, self.by, raise_error=True)
+            
+            if len(set(self.by).intersection(set(feature_list))) != 0:
+                raise ValueError("Columns in 'by' cannot be considered features.")
+        
         # Add targets to the list of features if they were not included
         self.features = list(set(feature_list + target_list))
 
         # Type checking and inference
         data = normalize_data(data, self.features)
 
-        self._data = data
+        self.data = data
         self.meta: Dict[str, FeatureMeta] = {}
         self._infer_feature_types(force_types=force_types)
-
+        
+        
         self.normalizers: Dict[str, GroupbyNormalizer] = {}
         self.normalized: Dict[str, bool] = {}
+        if normalizers is not None:
+            for key, normalizer in normalizers:
+                self._add_normalizer(key, normalizer)
+            
 
     def get_data(
         self,
-        features_only: bool = False,
-        to_binary_indicators: Optional[Union[str, List[str]]] = None
+        features_only: bool = True,
+        to_binary_indicators: Optional[Union[str, List[str]]] = None,
     ) -> pd.DataFrame:
         """Get the features data.
 
         Parameters
         ----------
-        features_only: bool, default = False
+        features_only: bool, default = True
             Whether to return only the by and feature columns.
         to_indicators: str or list of str, optional
             Ordinal features to convert to categorical indicators.
@@ -224,7 +240,7 @@ class Features:
             Features data.
 
         """
-        data = self._data
+        data = self.data
         
         # Take only the feature columns
         if features_only:
@@ -251,7 +267,7 @@ class Features:
             List of all column names.
 
         """
-        return self._data.columns
+        return self.data.columns
 
     def feature_names(
         self,
@@ -310,7 +326,54 @@ class Features:
 
         """
         return [col for col, meta in self.meta.items() if meta.is_target()]
-
+    
+    def split_by_values(self, value_splits: List[np.ndarray]):
+        on = self.by[0]
+        unique = self.data[on].unique()
+        unique.sort()
+        
+        all_vals = np.concatenate(value_splits)
+        all_vals.sort()
+        
+        if not np.array_equal(unique, all_vals):
+            raise ValueError("Invalid split values.")
+        
+        datas = []
+        for split in value_splits:
+            data_copy = self.data.copy()
+            datas.append(data_copy[data_copy[on].isin(split)])
+        
+        save_data = self.data
+        self.data = None
+        splits = [copy.deepcopy(self) for i in range(len(value_splits))]
+        for i, split in enumerate(splits):
+            split.data = datas[i]
+        self.data = save_data
+        
+        return tuple(splits)
+    
+    def split(
+        self,
+        fractions: Optional[Union[float, List[float]]] = None,
+        randomize: bool = True,
+        seed: int = None,
+    ):  
+        value_splits = self._compute_value_splits(fractions, randomize, seed)
+        return self.split_by_values(value_splits)
+    
+    def compute_value_splits(
+        self,
+        fractions: Optional[Union[float, List[float]]] = None,
+        randomize: bool = True,
+        seed: int = None,
+    ):  
+        on = self.by[0]
+        unique = self.data[on].unique()
+        unique.sort()
+        idx_splits = list(split_idx(fractions, len(unique), randomize=randomize, seed=seed))
+        value_splits = [np.take(unique, split) for split in idx_splits]
+        return value_splits
+    
     def _update_meta(self, meta_update: dict) -> None:
         """Update feature metadata.
 
@@ -374,7 +437,7 @@ class Features:
                 if FEATURE_INDICATOR_ATTR in fmeta:
                     self.features.append(col)
 
-            self._data = data
+            self.data = data
             self._update_meta(meta)
 
         return data
@@ -397,16 +460,16 @@ class Features:
             to_list_optional(force_types, none_to_empty=True)
         )
 
-        new_types = infer_types(self._data, infer_features)
+        new_types = infer_types(self.data, infer_features)
 
         # Force certain features to be specific types
         if force_types is not None:
             for feature, type_ in force_types.items():
                 new_types[feature] = type_
 
-        self._to_feature_types(self._data, new_types)
+        self._to_feature_types(self.data, new_types)
 
-    def add_normalizer(
+    def _add_normalizer(
         self,
         key: str,
         normalizer: GroupbyNormalizer,
@@ -428,7 +491,7 @@ class Features:
 
         by = normalizer.get_by()  # pylint: disable=invalid-name
         if by is not None:
-            has_columns(self._data, by, raise_error=True)
+            has_columns(self.data, by, raise_error=True)
 
         normalizer_map = normalizer.get_map()
         features = set(normalizer_map.keys())
@@ -459,7 +522,7 @@ class Features:
             )
 
         gbn = GroupbyNormalizer(normalizer_map, by)
-        gbn.fit(self._data)
+        gbn.fit(self.data)
         self.normalizers[key] = gbn
         self.normalized[key] = False
 
@@ -497,10 +560,10 @@ class Features:
             raise ValueError(f"Cannot normalize {key}. It has already been normalized.")
 
         gbn = self.normalizers[key]
-        data = gbn.transform(self._data)
+        data = gbn.transform(self.data)
 
         if inplace:
-            self._data = data
+            self.data = data
             self.normalized[key] = True
 
         return data
@@ -527,10 +590,10 @@ class Features:
             )
 
         gbn = self.normalizers[key]
-        data = gbn.inverse_transform(self._data)
+        data = gbn.inverse_transform(self.data)
 
         if inplace:
-            self._data = data
+            self.data = data
             self.normalized[key] = False
 
         return data
@@ -593,7 +656,7 @@ class Features:
             Processed save path for upstream use.
 
         """
-        return save_dataframe(self._data, save_path, file_format)
+        return save_dataframe(self.data, save_path, file_format)
 
 
 class TabularFeatures(Features):
@@ -603,14 +666,14 @@ class TabularFeatures(Features):
         self,
         data: pd.DataFrame,
         features: Union[str, List[str]],
-        by: Optional[str] = None,
+        by: str,
         targets: Optional[Union[str, List[str]]] = None,
         force_types: Optional[dict] = None,
     ):
         """Init."""
-        if by is not None and not isinstance(by, str):
+        if not isinstance(by, str):
             raise ValueError(
-                "Tabular features must have no index, or one index input as a string."
+                "Tabular features index input as a string representing a column."
             )
 
         super().__init__(
@@ -620,6 +683,36 @@ class TabularFeatures(Features):
             targets=targets,
             force_types=force_types,
         )
+    
+    def vectorize(self, **get_data_kwargs) -> Tuple:
+        """
+        
+        Parameters
+        ----------
+        **get_data_kwargs
+            Keyword arguments to be fed to get_data.
+        
+        Returns
+        -------
+        tuple
+            (data, by_map, feat_map), (pandas.DataFrame, dict, dict)
+            feat_map is the feature order and by_map is the order of
+            the by column, or None if no by was provided.
+        """
+        if "features_only" in get_data_kwargs:
+            raise ValueError(
+                "Cannot specify 'features_only'. It will be set to True by default."
+            )
+        
+        get_data_kwargs["features_only"] = True
+        
+        data = self.get_data(**get_data_kwargs)
+        
+        by_map = list(data[self.by[0]].values)
+        data = data.drop(self.by, axis=1)
+        feat_map = list(data.columns)
+        
+        return Vectorized(data.values, indexes=[by_map, feat_map])
 
     def plot_features(
         self,
@@ -636,9 +729,9 @@ class TabularFeatures(Features):
 
         """
         if features is None:
-            plot_histogram(self._data, self.feature_names)
+            plot_histogram(self.data, self.feature_names)
         else:
-            plot_histogram(self._data, features)
+            plot_histogram(self.data, features)
 
 
 class TemporalFeatures(Features):
@@ -688,9 +781,9 @@ class TemporalFeatures(Features):
 
         """
         if features is None:
-            plot_temporal_features(self._data, self.feature_names)
+            plot_temporal_features(self.data, self.feature_names)
         else:
-            plot_temporal_features(self._data, features)
+            plot_temporal_features(self.data, features)
 
     def aggregate(self) -> pd.DataFrame:
         """Aggregate the data.
@@ -715,4 +808,15 @@ class TemporalFeatures(Features):
                 f"The following columns are not features: {', '.join(nonexistent)}."
             )
 
-        return self.aggregator(self._data)
+        return self.aggregator(self.data)
+
+
+def split_features(
+    features: List[Union[Features, TabularFeatures, TemporalFeatures]],
+    fractions: Optional[Union[float, List[float]]] = None,
+    randomize: bool = True,
+    seed: int = None,
+) -> None:    
+    value_splits = features[0].compute_value_splits(fractions, randomize=randomize, seed=seed)
+    features = [feat.split_by_values(value_splits) for feat in features]
+    return tuple(features)

cyclops/processors/feature/normalization.py
@@ -1,6 +1,6 @@
 """Feature normalization."""
 
-from typing import List, Optional, Union
+from typing import Dict, List, Optional, Union, Tuple
 
 import numpy as np
 import pandas as pd
@@ -10,7 +10,18 @@ from cyclops.processors.constants import MIN_MAX, STANDARD
 from cyclops.processors.util import has_columns, has_range_index
 from cyclops.utils.common import to_list_optional
 
+METHOD_MAP = {STANDARD: StandardScaler, MIN_MAX: MinMaxScaler}
 
+def index_axis(ind: int, axis: int, shape: Tuple) -> Tuple:
+    index = [slice(None)]*len(shape)
+    index[axis] = ind
+    return tuple(index)
+
+def index_axis_ranged(start: int, stop: int, axis:int, shape: Tuple) -> Tuple:
+    index = [slice(None)]*len(shape)
+    index[axis] = slice(start, stop)
+    return tuple(index)
+    
 class SklearnNormalizer:
     """Sklearn normalizer wrapper.
 
@@ -33,65 +44,95 @@ class SklearnNormalizer:
 
         """
         self.method = method
-        method_map = {STANDARD: StandardScaler, MIN_MAX: MinMaxScaler}
 
         # Raise an exception if the method string is not recognized.
-        if method not in method_map:
-            options = ", ".join(["'" + k + "'" for k in method_map])
+        if method not in METHOD_MAP:
+            options = ", ".join(["'" + k + "'" for k in METHOD_MAP])
             raise ValueError(
                 f"Method '{method}' is invalid, must be in: {', '.join(options)}."
             )
 
-        self.scaler = method_map[method]()
+        self.scaler = METHOD_MAP[method]()
 
-    def fit(self, series: pd.Series) -> None:
+    def fit(self, data: Union[np.ndarray, pd.Series]) -> None:
         """Fit the scaler.
 
         Parameters
         ----------
-        series: pandas.Series
-            Series with values over which to fit.
+        data: numpy.ndarray or pandas.Series
+            Data over which to fit.
 
         """
-        self.scaler.fit(series.values.reshape(-1, 1))
+        # Series
+        if isinstance(data, pd.Series):
+            self.scaler.fit(data.values.reshape(-1, 1))
+            return
+        
+        # Array
+        if len(data.shape) != 1:
+            raise ValueError("Data must be a 1D array.")
+        self.scaler.fit(data.reshape(-1, 1))
 
-    def transform(self, series: pd.Series) -> pd.Series:
+    def transform(self, data: Union[np.ndarray, pd.Series]) -> Union[np.ndarray, pd.Series]:
         """Apply normalization.
 
+        If a numpy.ndarray is given, a numpy.ndarray is returned. Similarly, if a
+        pandas.Series is given, a pandas.Series is returned.
+
         Parameters
         ----------
-        series: pandas.Series
-            Input series
+        data: numpy.ndarray or pandas.Series
+            Input data.
 
         Returns
         -------
-        pandas.Series
-            Normalized series.
+        numpy.ndarray or pandas.Series
+            Normalized data.
 
         """
-        return pd.Series(
-            np.squeeze(self.scaler.transform(series.values.reshape(-1, 1))),
-            index=series.index,
-        )
+        # Series
+        if isinstance(data, pd.Series):
+            return pd.Series(
+                np.squeeze(self.scaler.transform(data.values.reshape(-1, 1))),
+                index=data.index,
+            )
+        
+        # Array
+        #if len(data.shape) != 1:
+        #    raise ValueError("Data must be a 1D array.")
+        
+        return np.squeeze(self.scaler.transform(data.reshape(-1, 1)))
+        
 
-    def inverse_transform(self, series: pd.Series) -> pd.Series:
+    def inverse_transform(self, data: Union[np.ndarray, pd.Series]) -> Union[np.ndarray, pd.Series]:
         """Apply inverse normalization.
+        
+        If a numpy.ndarray is given, a numpy.ndarray is returned. Similarly, if a
+        pandas.Series is given, a pandas.Series is returned.
 
         Parameters
         ----------
-        series: pandas.Series
-            Input series
+        data: numpy.ndarray or pandas.Series
+            Input data.
 
         Returns
         -------
-        pandas.Series
-            Inversely normalized series.
+        numpy.ndarray or pandas.Series
+            Inversely normalized data.
 
         """
-        return pd.Series(
-            np.squeeze(self.scaler.inverse_transform(series.values.reshape(-1, 1))),
-            index=series.index,
-        )
+        # Series
+        if isinstance(data, pd.Series):
+            return pd.Series(
+                np.squeeze(self.scaler.inverse_transform(data.values.reshape(-1, 1))),
+                index=data.index,
+            )
+        
+        # Array
+        #if len(data.shape) != 1:
+        #    raise ValueError("Data must be a 1D array.")
+        
+        return np.squeeze(self.scaler.transform(data.reshape(-1, 1)))
 
     def __repr__(self):
         """Repr method.
@@ -231,8 +272,7 @@ class GroupbyNormalizer:
                 # Get normalizer object and transform
                 normalizer = self.normalizers.loc[group.index.values[0]][col]
                 group[col] = getattr(normalizer, method)(group[col])
-
-            return group
+            return group.reset_index()
 
         # Over columns
         if self.by is None:
@@ -241,12 +281,19 @@ class GroupbyNormalizer:
                 data[col] = getattr(normalizer, method)(data[col])
         # Over groups
         else:
-            data = data.reset_index()
+            # WARNING: The indexing here is very confusing, where if not done properly
+            # the function will hang without explanation... devs be warned!
+            if data.index.name == None:
+                data = data.reset_index(drop=True)
+            else:
+                data = data.reset_index()
+            
             data.set_index(self.by, inplace=True)
-            grouped = data.groupby(self.by)
-            data = grouped.apply(transform_group).reset_index()
-            data = data.set_index("index")
-            data = data.sort_index()
+            grouped = data.groupby(self.by, as_index=False, sort=False)
+            data = grouped.apply(transform_group)
+            data = data.reset_index(drop=True)
+            #data = data.set_index("index")
+            #data = data.sort_index()
 
         return data
 
@@ -281,3 +328,174 @@ class GroupbyNormalizer:
 
         """
         return self._transform_by_method(data, "inverse_transform")
+
+
+class VectorizedNormalizer:
+    """Perform normalization over a NumPy array.
+
+    Attributes
+    ----------
+    normalizer_map: dict
+        A map from the column name to the type of normalization, e.g.,
+        {"event_values": "standard"}
+    normalizers: pandas.DataFrame
+        The normalizer object information, where each column/group has a row
+        with a normalization object.
+
+    """
+
+    def __init__(
+        self,
+        axis: int,
+        normalization_method: Optional[str] = None,
+        normalizer_map: Optional[dict] = None,
+    ) -> None:
+        """Initialize."""
+        if normalization_method is None and normalizer_map is None:
+            raise ValueError(
+                ("Must specify normalization_method to normalize all features "
+                 "with the same method, or normalization_map to map specific "
+                 "indices to normalization methods.")
+            )
+        if normalization_method is not None and normalizer_map is not None:
+            raise ValueError(
+                "Cannot specify both normalization_method and normalization_map."
+            )
+        
+        self.axis = axis
+        self.normalization_method = normalization_method
+        self.normalizer_map = normalizer_map
+        self.normalizers = None
+
+    def get_map(self) -> dict:
+        """Get normalization mapping from features to type.
+
+        Returns
+        -------
+        dict
+            Normalization map.
+
+        """
+        return self.normalizer_map
+    
+    def _check_missing(self, feat_map: Dict[str, int]) -> None:
+        """Check if any of the normalizer_map features are missing a given feat_map.
+        
+        Parameters
+        ----------
+        feat_map: dict
+            Map from feature name to index in the normalizer's given axis.
+
+        """
+        missing = set(self.normalizer_map.keys()) - set(feat_map.keys())
+        if len(missing) != 0:
+            raise ValueError(f"Missing features {', '.join(missing)} in the data.")
+    
+    def fit(self, data: np.ndarray, feat_map: Dict[str, int]) -> None:
+        """Fit the normalizing objects.
+
+        Parameters
+        ----------
+        data: pandas.DataFrame
+            Data over which to fit.
+        feat_map: dict
+            Map from feature name to index in the normalizer's given axis.
+
+        """
+        if self.normalizer_map is None:
+            # Use the same normalization method for all features
+            self.normalizer_map = {feat: self.normalization_method for feat in feat_map.keys()}
+        else:
+            self._check_missing(feat_map)
+        
+        self.normalizers = {}
+        for feat, method in self.normalizer_map.items():
+            if isinstance(method, str):
+                normalizer = SklearnNormalizer(method)
+            else:
+                raise ValueError(
+                    """Must specify a string for the normalizer type.
+                    Passing normalizer objects not yet supported."""
+                )
+            
+            ind = feat_map[feat]
+            values = data[index_axis(ind, self.axis, data.shape)]
+            print("values", values)
+            values = values.flatten()
+            print("values.shape", values.shape)
+            normalizer.fit(values)
+            self.normalizers[feat] = normalizer
+
+    def _transform_by_method(self, data: np.ndarray, feat_map: Dict[str, int],  method: str) -> np.ndarray:
+        """Apply a method from the normalizer object to the data.
+
+        Parameters
+        ----------
+        data: numpy.ndarray
+            Data to transform.
+        feat_map: dict
+            Map from feature name to index in the normalizer's given axis.
+        method: str
+            Name of normalizer method to apply.
+
+        Returns
+        -------
+        numpy.ndarray
+            The data with the method applied.
+
+        """
+        if self.normalizers is None:
+            raise ValueError("Must first fit the normalizers.")
+            
+        self._check_missing(feat_map)
+
+        for feat, normalizer in self.normalizers.items():
+            ind = feat_map[feat]
+            data_indexing = index_axis(ind, self.axis, data.shape)
+            values = data[data_indexing]
+            prev_shape = values.shape
+            print("feat\n", feat)
+            print("values\n", values)
+            values = values.flatten()
+            normalized = getattr(normalizer, method)(values).reshape(prev_shape)
+            print("normalized\n", normalized)
+            data[data_indexing] = normalized
+            print("\n")
+        return data
+
+    def transform(self, data: np.ndarray, feat_map: Dict[str, int]):
+        """Normalize the data.
+
+        Parameters
+        ----------
+        data: numpy.ndarray
+            Data to transform.
+        feat_map: dict
+            Map from feature name to index in the normalizer's given axis.
+
+        Returns
+        -------
+        numpy.ndarray
+            The normalized data.
+
+        """
+        return self._transform_by_method(data, feat_map, "transform")
+
+    def inverse_transform(self, data: np.ndarray, feat_map: Dict[str, int]):
+        """Inversely normalize the data.
+
+        Parameters
+        ----------
+        data: numpy.ndarray
+            Data to transform.
+        feat_map: dict
+            Map from feature name to index in the normalizer's given axis.
+
+        Returns
+        -------
+        numpy.ndarray
+            The inversely normalized data.
+
+        """
+        return self._transform_by_method(data, feat_map, "inverse_transform")
+        

cyclops/processors/feature/split.py
@@ -3,8 +3,53 @@
 from typing import List, Optional, Tuple, Union
 
 import numpy as np
+import pandas as pd
 
 from cyclops.utils.common import to_list
+
+
+def intersect_datasets(
+    datas: List[pd.DataFrame],
+    on: str,
+    sort: bool = True,
+) -> Tuple:
+    """Perform an intersection across datasets over a column.
+    
+    This can be used to align dataset samples e.g., aligning encounters for a tabular
+    and temporal dataset.
+    
+    Parameters
+    ----------
+    datas: list of pandas.DataFrame
+        List of datasets.
+    on: str
+        The column on which to perform the intersection.
+    sort: bool, default = True
+        Whether to sort the values in each dataset by the on column.
+    
+    Returns
+    -------
+    tuple
+        A tuple of the processed datasets.
+
+    """
+    # Concatenate the unique values in each dataset and count how many of each
+    unique, counts = np.unique(
+        np.concatenate([data[on].unique() for data in datas]),
+        return_counts=True
+    )
+
+    # If a count is equal to the length of datasets, it must exist in every dataset
+    intersect = unique[counts == len(datas)]
+
+    # Intersect on these unique values
+    for i, data in enumerate(datas):
+        data = data[data[on].isin(intersect)]
+        if sort:
+            data = data.sort_values(on)
+        datas[i] = data
+
+    return datas
 
 
 def fractions_to_split(
@@ -88,7 +133,41 @@ def split_idx(
     return tuple(np.split(idx, split))
 
 
-def split_data(
+def split_datasets_by_idx(
+    datasets: Union[np.ndarray, List[np.ndarray]],
+    idx_splits: Tuple,
+    axes: Optional[Union[int, List[int]]] = None,
+):
+    if isinstance(datasets, np.ndarray):
+        datasets = [datasets]
+
+    if axes is None:
+        axes_list = [0] * len(datasets)
+    else:
+        axes_list = to_list(axes)
+    
+    splits = []  # type: ignore
+    # For each dataset
+    for i, data in enumerate(datasets):
+        splits.append([])
+        # For each split
+        for idx in idx_splits:
+            # Reshape idx to have same number of dimensions as the data
+            shape = [1] * len(data.shape)
+            shape[axes_list[i]] = len(idx)
+            idx = idx.reshape(shape)
+
+            # Sample new dataset split
+            splits[-1].append(np.take_along_axis(data, idx, axis=axes_list[i]))
+        splits[-1] = tuple(splits[-1])
+
+    if len(splits) == 1:
+        return splits[0]
+
+    return tuple(splits)
+
+
+def split_datasets(
     datasets: Union[np.ndarray, List[np.ndarray]],
     fractions: Union[float, List[float]],
     axes: Optional[Union[int, List[int]]] = None,
@@ -129,22 +208,4 @@ def split_data(
 
     idx_splits = split_idx(fractions, sizes[0], randomize=randomize, seed=seed)
 
-    splits = []  # type: ignore
-    # For each dataset
-    for i, data in enumerate(datasets):
-        splits.append([])
-        # For each split
-        for idx in idx_splits:
-            # Reshape idx to have same number of dimensions as the data
-            shape = [1] * len(data.shape)
-            shape[axes_list[i]] = len(idx)
-            idx = idx.reshape(shape)
-
-            # Sample new dataset split
-            splits[-1].append(np.take_along_axis(data, idx, axis=axes_list[i]))
-        splits[-1] = tuple(splits[-1])
-
-    if len(splits) == 1:
-        return splits[0]
-
-    return tuple(splits)
+    return split_datasets_by_idx(datasets, idx_splits)