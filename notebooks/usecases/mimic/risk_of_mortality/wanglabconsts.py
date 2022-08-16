47144e095babc871688a958a7b2b069616a62ef7
Kaden McKeen <mckeen.kaden.school@hotmail.com>
2022-08-16 13:00:34 -0400

    Begin implementing the WangLab use case using the generalizable pipeline. Changed names of constants files to follow PEP-8 standards.

--- notebooks/usecases/mimic/risk_of_mortality/wanglabconsts.py
+++ notebooks/usecases/mimic/risk_of_mortality/wanglabconsts.py
@@ -1,10 +1,66 @@
-"""WangLab cardiac use case constants."""
+"""WangLab use case constants."""
 
 import numpy as np
 
+from cyclops.processors.column_names import (
+    HOSPITAL_ID,
+    AGE,
+    SEX,
+    DIAGNOSIS_TRAJECTORY,
+)
+from cyclops.processors.constants import TARGETS
+from cyclops.utils.file import join, process_dir_save_path
+
+CONST_NAME = "wanglab"
+ROOT_DIR = process_dir_save_path("./wanglab_data")
+
 OUTCOME_DEATH = "outcome_death"
 OUTCOME_EDEMA = "outcome_edema"
+SPLIT_FRACTIONS = [0.8, 0.1, 0.1]
 
+ENCOUNTERS_FILE = join(ROOT_DIR, "encounters.parquet")
+AGGREGATED_FILE = join(ROOT_DIR, "aggregated.parquet")
+TAB_FEATURES_FILE = join(ROOT_DIR, "tab_features.pkl")
+TAB_VECTORIZED_FILE = join(ROOT_DIR, "tab_vectorized.pkl")
+TEMP_VECTORIZED_FILE = join(ROOT_DIR, "temp_vectorized.pkl")
+
+# Tabular
+TAB_TARGETS = [OUTCOME_DEATH, OUTCOME_EDEMA]
+TAB_FEATURES = [
+    HOSPITAL_ID,
+    AGE,
+    SEX,
+    DIAGNOSIS_TRAJECTORY,
+    OUTCOME_DEATH,
+    OUTCOME_EDEMA,
+    "readmission",
+    "from_nursing_home_mapped",
+    "from_acute_care_institution_mapped",
+    "los_derived",
+    "prev_encounter_count",
+] + TAB_TARGETS
+
+# Temporal
+TIMESTEP_SIZE = 24
+WINDOW_DURATION = 144
+PREDICT_OFFSET = 24
+
+TOP_N_EVENTS = 150
+
+TEMP_TARGETS = [OUTCOME_DEATH]
+
+QUERIED_DIR = process_dir_save_path(join(ROOT_DIR, "0queried"))
+CLEANED_DIR = process_dir_save_path(join(ROOT_DIR, "1cleaned"))
+AGGREGATED_DIR = process_dir_save_path(join(ROOT_DIR, "2agg"))
+VECTORIZED_DIR = process_dir_save_path(join(ROOT_DIR, "3vec"))
+
+# Saving final vectorized
+FINAL_VECTORIZED = process_dir_save_path(join(ROOT_DIR, "4final"))
+TAB_UNALIGNED = join(FINAL_VECTORIZED, "unaligned_")
+TAB_VEC_COMB = join(FINAL_VECTORIZED, "aligned_")
+
+
+# Querying constants
 BEFORE_DATE = "2020-01-23"
 
 SEXES = ["M", "F"]
