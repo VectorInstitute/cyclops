dc17f77c2b8ff228f36b05bbc8b90cbeff6af4b4
Amrit K <amritk@vectorinstitute.ai>
2022-10-12 16:27:07 -0400

    Format nbs

--- drift_detection/baseline_models/static/gbt.py
+++ drift_detection/baseline_models/static/gbt.py
@@ -9,6 +9,7 @@ def fit_gbt(X, Y, Xv, Yv):
     best_g = None
     best_score = 0
     best_model = None
+    
     for n in [3, 5, 7, 9, 11]:
         for g in [0.5, 1, 1.5, 2, 5]:
             m = XGBClassifier(
@@ -21,9 +22,8 @@ def fit_gbt(X, Y, Xv, Yv):
                 seed=42,
                 use_label_encoder=False,
             )
-            # print("Fitting model with n: {} and g: {}".format(n, g))
             m.fit(X, Y)
-            Pv = m.predict_proba(Xv)[:, 1]
+            Pv = m.predict(Xv)
             score = roc_auc_score(Yv, Pv)
             if score > best_score:
                 best_score = score
@@ -32,4 +32,5 @@ def fit_gbt(X, Y, Xv, Yv):
                 best_g = g
     print("Best g:", best_g)
     print("Best n:", best_n)
+    
     return best_model
