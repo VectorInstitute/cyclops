c74566cf6c44390da04c640ccd3d019bfa23e5d4
Amrit K <amritk@vectorinstitute.ai>
2022-10-19 15:10:40 -0400

    Add additional tests for query process fns.

--- tests/cyclops/query/test_process.py
+++ tests/cyclops/query/test_process.py
@@ -3,7 +3,15 @@
 import pytest
 from sqlalchemy import column, select
 
-from cyclops.query.process import process_checks
+from cyclops.query.process import (
+    QAP,
+    ConditionIn,
+    ConditionSubstring,
+    Drop,
+    Rename,
+    process_checks,
+    process_operations,
+)
 from cyclops.query.util import process_column
 
 
@@ -19,3 +27,32 @@ def test_process_checks(test_table):  # pylint: disable=redefined-outer-name
     process_checks(test_table, cols=["a"], cols_not_in=["d"], timestamp_cols=["a"])
     with pytest.raises(ValueError):
         process_checks(test_table, cols_not_in=["a"])
+
+
+def test_process_operations(test_table):  # pylint: disable=redefined-outer-name
+    """Test process_operations fn."""
+    process_kwargs = {"b_args": "cat", "c_args": ["dog"]}
+    operations = [
+        (
+            ConditionIn,
+            ["b", QAP("b_args")],
+            {"to_str": True},
+        ),
+        (ConditionSubstring, ["c", QAP("c_args")], {}),
+    ]
+    table = process_operations(test_table, operations, process_kwargs)
+    query_lines = str(table).splitlines()
+    assert query_lines[0] == "SELECT anon_1.a, anon_1.b, anon_1.c "
+    assert query_lines[-1] == "WHERE lower(CAST(anon_1.c AS VARCHAR)) LIKE :lower_1"
+
+
+def test_drop(test_table):  # pylint: disable=redefined-outer-name
+    """Test Drop operation."""
+    table = Drop("a")(test_table)
+    assert str(table).splitlines()[0] == "SELECT anon_1.b, anon_1.c "
+
+
+def test_rename(test_table):  # pylint: disable=redefined-outer-name
+    """Test Rename operation."""
+    table = Rename({"a": "d"})(test_table)
+    assert str(table).splitlines()[0] == "SELECT anon_1.a AS d, anon_1.b, anon_1.c "
