fix return value for binary precision and recall

cyclops/evaluation/metrics/functional/precision_recall.py
@@ -57,8 +57,8 @@ def _precision_recall_reduce(  # pylint: disable=invalid-name, too-many-argument
     modifier = "predicted" if metric == "precision" else "true"
 
     score = _prf_divide(
-        numerator,
-        denominator,
+        np.array(numerator) if np.isscalar(tp) else numerator,
+        np.array(denominator) if np.isscalar(tp) else denominator,
         metric,
         modifier,
         average,
@@ -247,12 +247,6 @@ def binary_precision(  # pylint: disable=too-many-arguments
         threshold=threshold,
         sample_weight=sample_weight,
     )
-
-    if tp.ndim == 0:
-        tp = np.array([tp])
-        fp = np.array([fp])
-        tn = np.array([tn])
-        fn = np.array([fn])
 
     precision_score = _precision_recall_reduce(
         tp,
@@ -599,12 +593,6 @@ def binary_recall(  # pylint: disable=too-many-arguments
         threshold=threshold,
         sample_weight=sample_weight,
     )
-
-    if tp.ndim == 0:
-        tp = np.expand_dims(tp, axis=-1)
-        fp = np.expand_dims(fp, axis=-1)
-        tn = np.expand_dims(tn, axis=-1)
-        fn = np.expand_dims(fn, axis=-1)
 
     recall_score = _precision_recall_reduce(
         tp,