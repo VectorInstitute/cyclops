038bf2793e353774619f897bd75d36471a216d92
mahshidaln <mahshid.alinoori@gmail.com>
2022-11-07 21:06:05 -0500

    Add neural networks

---
+++ models/neural_nets/mlp.py
@@ -0,0 +1,99 @@
+"""MLP models."""
+
+import math
+
+import torch
+from torch import nn
+
+from models.utils import ACTIVATIONS
+
+
+class MLPModel(nn.Module):  # pylint: disable=too-few-public-methods
+    """A Multi-Layer Perceptron (MLP).
+
+    Also known as a Fully-Connected Network (FCN). This implementation assumes that all
+    hidden layers have the same hidden size and the same activation function.
+
+    """
+
+    def __init__(  # pylint: disable=R0913
+        self,
+        device: torch.device,
+        input_dim: int,
+        hidden_dims: list,
+        layer_dim: int,
+        output_dim: int,
+        activation: str = "relu",
+    ):
+        """Instantiate model.
+
+        Parameters
+        ----------
+        num_layers: int
+            Number of layers in the network.
+        in_dim: int
+            Size of the input sample.
+        hidden_dims: list
+            Sizes of the hidden layers.
+        out_dim: int
+            Size of the output.
+        activation: str, optional
+            Activation function.
+
+        """
+        super().__init__()
+        self.device = device
+        self.input_dim = input_dim
+        self.hidden_dims = hidden_dims
+        self.layer_dim = layer_dim
+        self.output_dim = output_dim
+        self.activation = ACTIVATIONS[activation]
+
+        layers = []
+        for i in range(self.layer_dim - 1):
+            layers.extend(
+                self._layer(
+                    self.hidden_dims[i] if i > 0 else input_dim,
+                    self.hidden_dims[i + 1],
+                    activation,
+                )
+            )
+        layers.extend(self._layer(hidden_dims[i + 1], output_dim))
+
+        self.model = nn.Sequential(*layers)
+
+        # Initialize weights.
+        for module in self.modules():
+            if isinstance(module, nn.Linear):
+                nn.init.kaiming_uniform_(module.weight, a=math.sqrt(5))
+                fan_in, _ = nn.init._calculate_fan_in_and_fan_out(module.weight)
+                bound = 1 / math.sqrt(fan_in)
+                nn.init.uniform_(module.bias, -bound, bound)
+
+    def _layer(self, in_dim, out_dim, activation=None):
+        if activation:
+            layer = [
+                nn.Linear(in_dim, out_dim),
+                self.activation,
+            ]
+        else:
+            layer = [
+                nn.Linear(in_dim, out_dim),
+            ]
+        return layer
+
+    def forward(self, input_: torch.Tensor):
+        """Forward pass.
+
+        Parameters
+        ----------
+        input_: torch.Tensor
+            Input to the model.
+
+        Returns
+        -------
+        torch.Tensor
+            Model output.
+
+        """
+        return self.model(input_)
---
+++ models/neural_nets/rnn.py
@@ -0,0 +1,76 @@
+"""RNN models."""
+from typing import Optional
+
+import torch
+from torch import nn
+
+# pylint: disable=invalid-name, too-many-arguments
+
+
+class RNNModel(nn.Module):
+    """RNN Class."""
+
+    def __init__(
+        self,
+        device: torch.device,
+        input_dim: int,
+        hidden_dim: int,
+        layer_dim: int,
+        output_dim: int,
+        dropout_prob: float,
+        last_timestep_only: Optional[bool] = False,
+    ) -> None:
+        """Initialize model.
+
+        Parameters
+        ----------
+        device : torch.device
+            The device to allocate
+        input_dim : int
+            Number of input features
+        hidden_dim : int
+            Number of features in the hidden state
+        layer_dim : int
+            Number of hidden layers
+        output_dim : int
+            Dimension of output
+        dropout_prob : float
+            Dropout probability for dropout layer
+        last_timestep_only : Optional[bool], optional
+            Keep the last timestep, by default False
+
+        """
+        super().__init__()
+        self.device = device
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+        self.rnn = nn.RNN(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.last_timestep_only = last_timestep_only
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input to the model
+
+        Returns
+        -------
+        torch.Tensor
+            Model output
+
+        """
+        h0 = (
+            torch.zeros(self.layer_dim, x.size(0), self.hidden_dim)
+            .requires_grad_()
+            .to(self.device)
+        )
+        out, h0 = self.rnn(x, h0.detach())
+        if self.last_timestep_only:
+            out = out[:, -1, :]
+        out = self.fc(out)
+        return out
