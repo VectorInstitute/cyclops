b0062437d7c3d87a67809ff34070f738dff02d2c
mahshidaln <mahshid.alinoori@gmail.com>
2022-11-07 21:15:42 -0500

    Add models constants

---
+++ models/constants.py
@@ -0,0 +1,14 @@
+"""Model library constants."""
+from cyclops.utils.file import join
+
+USE_CASES = {
+    "mortality_decompensation": ["gemini", "mimiciv"],
+    "delirium": ["gemini", "mimiciv"],
+}
+DATASETS = ["mimiciv", "gemini"]
+DATA_TYPES = ["tabular", "temporal", "combined"]
+
+TASKS = {"binary_classification": ["mortality_decompensation", "delirium"]}
+
+CONFIG_FILE = join("configs", "models.yaml")
+SAVE_DIR = join("/mnt", "cyclops", "checkpoints")
