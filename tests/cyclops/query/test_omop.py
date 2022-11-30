17d6695ae71f633efe53980ceb11059631eba126
Amrit Krishnan <amritk@vectorinstitute.ai>
2022-11-30 12:47:39 -0500

    Update concept map joins in OMOP query API to be outer joins (#315)
    * Update concept map joins in OMOP query API to be outer joins
    
    * Remove synthea use case

--- tests/cyclops/query/test_omop.py
+++ tests/cyclops/query/test_omop.py
@@ -2,12 +2,12 @@
 
 import pytest
 
-from cyclops.query.omop import VISIT_OCCURRENCE, OMOPQuerier
+from cyclops.query.omop import OMOPQuerier
 
 
 @pytest.mark.integration_test
 def test_omop_querier():
     """Test OMOPQuerier."""
     synthea = OMOPQuerier("cdm_synthea10", ["database=synthea"])
-    visits = synthea.get_interface(synthea.get_table(VISIT_OCCURRENCE)).run()
+    visits = synthea.visit_occurrence().run()
     assert len(visits) == 4856
