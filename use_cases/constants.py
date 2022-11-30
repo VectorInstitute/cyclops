8f0d8ab6a221a955f27493c4630247c2b2a93b3b
mahshidaln <mahshid.alinoori@gmail.com>
2022-11-30 17:08:11 -0500

    Add use-case constants

---
+++ use_cases/constants.py
@@ -0,0 +1,8 @@
+"""Use-cases constants."""
+
+USE_CASES = {
+    "mortality_decompensation": ["gemini", "mimiciv"],
+    "delirium": ["gemini", "mimiciv"],
+}
+DATASETS = ["mimiciv", "gemini"]
+DATA_TYPES = ["tabular", "temporal", "combined"]
