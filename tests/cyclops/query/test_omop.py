57d77c4581d7e823cb0152c885c253fbe5d00616
Amrit Krishnan <amritk@vectorinstitute.ai>
2022-12-13 15:59:37 -0500

    Update integration testing dataset, formalizing its generation (#337)

--- tests/cyclops/query/test_omop.py
+++ tests/cyclops/query/test_omop.py
@@ -8,14 +8,14 @@ from cyclops.query.omop import OMOPQuerier
 @pytest.mark.integration_test
 def test_omop_querier():
     """Test OMOPQuerier."""
-    synthea = OMOPQuerier("cdm_synthea10", ["database=synthea"])
+    synthea = OMOPQuerier("cdm_synthea10", ["database=synthea_integration_test"])
     visits = synthea.visit_occurrence().run()
     persons = synthea.person().run()
     observations = synthea.observation().run()
     measurements = synthea.measurement().run()
     visit_details = synthea.visit_detail().run()
-    assert len(visits) == 4856
-    assert len(visit_details) == 4856
+    assert len(visits) == 4115
+    assert len(visit_details) == 4115
     assert len(persons) == 109
-    assert len(observations) == 19671
-    assert len(measurements) == 20233
+    assert len(observations) == 16169
+    assert len(measurements) == 19373
