6f7bef08da147d90bfca892c8125c388233e96d7
Amrit K <amritk@vectorinstitute.ai>
2023-01-16 13:50:57 -0500

    Add query API tutorial nb for synthea

--- cyclops/query/__init__.py
+++ cyclops/query/__init__.py
@@ -1,1 +1,5 @@
-"""Queries package."""
+"""Query package."""
+
+from cyclops.query.gemini import GEMINIQuerier
+from cyclops.query.mimiciv import MIMICIVQuerier
+from cyclops.query.omop import OMOPQuerier
