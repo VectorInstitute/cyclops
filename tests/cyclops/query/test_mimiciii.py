a506efa62a821b09b5c4da94c9f23ddc40005da9
Amrit K <amritk@vectorinstitute.ai>
2023-03-21 12:42:24 -0400

    Add simple integration test for MIMICIII dataset API funcs

--- tests/cyclops/query/test_mimiciii.py
+++ tests/cyclops/query/test_mimiciii.py
@@ -8,8 +8,27 @@ from cyclops.query import MIMICIIIQuerier
 @pytest.mark.integration_test
 def test_mimiciii_querier():
     """Test MIMICIIIQuerier."""
-    querier = MIMICIIIQuerier()
+    querier = MIMICIIIQuerier(
+        dbms="postgresql",
+        port=5432,
+        host="localhost",
+        database="mimiciii",
+        user="postgres",
+        password="pwd",
+    )
     custom_tables = querier.list_custom_tables()
     assert "diagnoses" in custom_tables
     assert "labevents" in custom_tables
     assert "chartevents" in custom_tables
+
+    diagnoses = querier.diagnoses().run(limit=10)
+    assert len(diagnoses) == 10
+    assert "long_title" in diagnoses
+
+    labevents = querier.labevents().run(limit=10)
+    assert len(labevents) == 10
+    assert "itemid" in labevents
+
+    chartevents = querier.chartevents().run(limit=10)
+    assert len(chartevents) == 10
+    assert "itemid" in chartevents
