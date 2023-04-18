59b025b28de12edf98123bff20dddd3ffd05e8fb
Amrit Krishnan <amritk@vectorinstitute.ai>
2023-04-18 11:29:39 -0400

    Add method for listing columns in a given table (#412)

--- tests/cyclops/query/test_base.py
+++ tests/cyclops/query/test_base.py
@@ -1,8 +1,15 @@
-"""Test base dataset querier."""
+"""Test base dataset querier, using OMOPQuerier as an example."""
 
-from cyclops.query.base import DatasetQuerier
+import pytest
+
+from cyclops.query import OMOPQuerier
 
 
+@pytest.mark.integration_test
 def test_dataset_querier():
-    """Test dataset querier instantiation."""
-    _ = DatasetQuerier()
+    """Test base querier methods using OMOPQuerier."""
+    querier = OMOPQuerier("cdm_synthea10", database="synthea_integration_test")
+    assert len(querier.list_tables()) == 69
+    visit_occrrence_columns = querier.list_columns("cdm_synthea10", "visit_occurrence")
+    assert len(visit_occrrence_columns) == 17
+    assert "visit_occurrence_id" in visit_occrrence_columns
