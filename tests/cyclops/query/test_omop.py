7edfb386ae36efaf57c8176e0e79c2fe7e25cfbe
Amrit K <amritk@vectorinstitute.ai>
2023-07-27 12:52:51 -0400

    Fix integration tests

--- tests/cyclops/query/test_omop.py
+++ tests/cyclops/query/test_omop.py
@@ -29,12 +29,12 @@ def test_omop_querier_synthea():
         querier.cdm_synthea10.condition_occurrence().run()  # pylint: disable=no-member
     )
     assert len(persons) == 54
-    assert len(visits) == 1620
-    assert len(visit_details) == 4115
-    assert len(observations) == 16169
-    assert len(measurements) == 19373
+    assert len(visits) == 1798
+    assert len(visit_details) == 4320
+    assert len(observations) == 17202
+    assert len(measurements) == 19994
     assert len(providers) == 212
-    assert len(conditions) == 1363
+    assert len(conditions) == 1419
 
 
 @pytest.mark.integration_test
--- tests/cyclops/query/test_orm.py
+++ tests/cyclops/query/test_orm.py
@@ -17,10 +17,10 @@ def test_omop_querier():
     visits_query = querier.visit_occurrence().query
     db_.save_query_to_csv(visits_query, "visits.csv")
     visits_df = pd.read_csv("visits.csv")
-    assert len(visits_df) == 4115
+    assert len(visits_df) == 4320
     os.remove("visits.csv")
 
     db_.save_query_to_parquet(visits_query, "visits.parquet")
     visits_df = pd.read_parquet("visits.parquet")
-    assert len(visits_df) == 4115
+    assert len(visits_df) == 4320
     os.remove("visits.parquet")
