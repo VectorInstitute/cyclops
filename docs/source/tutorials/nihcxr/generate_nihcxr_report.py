6f542468047702d5226a3b1634d4cd7f603097d0
Ali Kore <akore0x5f@gmail.com>
2023-09-14 15:34:33 -0400

    add val split to nihcxr, create nihcxr report generation script

--- docs/source/tutorials/nihcxr/generate_nihcxr_report.py
+++ docs/source/tutorials/nihcxr/generate_nihcxr_report.py
@@ -1,14 +1,16 @@
 """Chest X-ray Disease Classification."""
 
-# get args from command line
-import argparse
+import os
+import shutil
+from datetime import date
 from functools import partial
-from typing import Any, Dict, List, Optional
+from pathlib import Path
 
 import numpy as np
-import numpy.typing as npt
 import plotly.express as px
-from monai.transforms import Compose, Lambdad, Resized  # type: ignore[attr-defined]
+import plotly.graph_objects as go
+from dateutil.relativedelta import relativedelta
+from monai.transforms import Compose, Lambdad, Resized
 from torchxrayvision.models import DenseNet
 
 from cyclops.data.loader import load_nihcxr
@@ -18,63 +20,49 @@ from cyclops.data.slicer import (
 )
 from cyclops.data.utils import apply_transforms
 from cyclops.evaluate import evaluator
+from cyclops.evaluate.fairness import evaluate_fairness  # noqa: E402
 from cyclops.evaluate.metrics.factory import create_metric
-from cyclops.evaluate.metrics.stat_scores import MultilabelStatScores
-from cyclops.models.wrappers import PTModel  # type: ignore[attr-defined]
-from cyclops.report import ModelCardReport  # type: ignore[attr-defined]
+from cyclops.models.wrappers import PTModel
+from cyclops.report import ModelCardReport
 
+# get args from command line
+import argparse
 
 parser = argparse.ArgumentParser()
-
-parser.add_argument("--sample_size", type=int, default=1000)
-parser.add_argument("--synthetic_timestamp", type=str, default=None)
-parser.add_argument("--split", type=str, default="val")
-parser.add_argument("--seed", type=int, default=42)
-
-args = parser.parse_args()
+parser.add_argument("--output_dir", type=str, default="cyclops_reports")
+parser.add_argument("--report_type", type=str, default="model_card", choices=["baseline", "periodic"])
 
 report = ModelCardReport()
 
 data_dir = "/mnt/data/clinical_datasets/NIHCXR"
-nih_ds = load_nihcxr(data_dir)[args.split]
-
-# select a subset of the data
-np.random.seed(args.seed)
-nih_ds = nih_ds.select(np.random.choice(len(nih_ds), args.sample_size))
+nih_ds = load_nihcxr(data_dir)["test"]
+nih_ds = nih_ds.select(range(4000))
 
 transforms = Compose(
     [
         Resized(
-            keys=("image",),
-            spatial_size=(224, 224),
-            allow_missing_keys=True,
+            keys=("image",), spatial_size=(224, 224), allow_missing_keys=True,
         ),
         Lambdad(
             keys=("image",),
             func=lambda x: ((2 * (x / 255.0)) - 1.0) * 1024,
             allow_missing_keys=True,
         ),
-        Lambdad(
-            ("image",),
-            func=lambda x: np.mean(x, axis=0)[np.newaxis, :] if x.shape[0] != 1 else x,
-        ),
+        Lambdad(("image",), func=lambda x: np.mean(x, axis=0)[np.newaxis, :] if x.shape[0] != 1 else x),
     ],
 )
-pathologies = DenseNet(weights="densenet121-res224-nih").pathologies[:14]
 
 model = PTModel(DenseNet(weights="densenet121-res224-nih"))
-model.initialize()  # type: ignore
-nih_ds = model.predict(
-    nih_ds,
-    feature_columns=["image"],
-    transforms=partial(apply_transforms, transforms=transforms),
-    model_name="densenet",
-)
+model.initialize()
+nih_ds = model.predict(nih_ds,
+                   feature_columns=["image"],
+                   transforms=partial(apply_transforms, transforms=transforms),
+                   model_name="densenet",
+              )
 
 # remove any rows with No Finding == 1
 nih_ds = nih_ds.filter(
-    partial(filter_value, column_name="No Finding", value=1, negate=True),
-    batched=True,
+    partial(filter_value, column_name="No Finding", value=1, negate=True), batched=True,
 )
 
 # remove the No Finding column and adjust the predictions to account for it
@@ -85,86 +73,28 @@ nih_ds = nih_ds.map(
     remove_columns=["No Finding"],
 )
 
+
+pathologies = model.model.pathologies[:14]
+
+auroc = create_metric(
+    metric_name="auroc",
+    task="multilabel",
+    num_labels=len(pathologies),
+    thresholds=np.arange(0, 1, 0.01),
+)
+
 # define the slices
-slices_sex = [
+slices = [
     {"Patient Gender": {"value": "M"}},
     {"Patient Gender": {"value": "F"}},
 ]
 
-
-class MultilabelPositivePredictiveValue(
-    MultilabelStatScores,
-    registry_key="positive_predictive_value",
-):
-    """Compute the recall score for multilabel classification tasks."""
-
-    def __init__(
-        self,
-        num_labels: int,
-        threshold: float = 0.5,
-        top_k: Optional[int] = None,
-    ) -> None:
-        """Initialize the metric."""
-        super().__init__(
-            num_labels=num_labels,
-            threshold=threshold,
-            top_k=top_k,
-            labelwise=True,
-        )
-
-    def compute(self) -> npt.NDArray[np.int_]:
-        """Compute the recall score from the state."""
-        tp, fp, tn, fn = self._final_state()
-        return tp / (tp + fp)  # type: ignore[return-value]
-
-
-class MultilabelNegativePredictiveValue(
-    MultilabelStatScores,
-    registry_key="negative_predictive_value",
-):
-    """Compute the recall score for multilabel classification tasks."""
-
-    def __init__(
-        self,
-        num_labels: int,
-        threshold: float = 0.5,
-        top_k: Optional[int] = None,
-    ) -> None:
-        """Initialize the metric."""
-        super().__init__(
-            num_labels=num_labels,
-            threshold=threshold,
-            top_k=top_k,
-            labelwise=True,
-        )
-
-    def compute(self) -> npt.NDArray[np.int_]:
-        """Compute the recall score from the state."""
-        tp, fp, tn, fn = self._final_state()
-        return tn / (tn + fn)  # type: ignore[return-value]
-
-
-ppv = MultilabelPositivePredictiveValue(num_labels=len(pathologies))
-
-npv = MultilabelNegativePredictiveValue(num_labels=len(pathologies))
-
-specificity = create_metric(
-    metric_name="specificity",
-    task="multilabel",  # type: ignore[arg-type]
-    num_labels=len(pathologies),  # type: ignore[arg-type]
-)
-
-sensitivity = create_metric(
-    metric_name="sensitivity",
-    task="multilabel",  # type: ignore[arg-type]
-    num_labels=len(pathologies),  # type: ignore[arg-type]
-)
 # create the slice functions
-slice_spec = SliceSpec(spec_list=slices_sex)
+slice_spec = SliceSpec(spec_list=slices)
 
 nih_eval_results_gender = evaluator.evaluate(
     dataset=nih_ds,
-    metrics=[ppv, npv, sensitivity, specificity],
+    metrics=auroc,
     feature_columns="image",
     target_columns=pathologies,
     prediction_column_prefix="predictions",
@@ -172,48 +102,89 @@ nih_eval_results_gender = evaluator.evaluate(
     slice_spec=slice_spec,
 )
 
+# plot the results
+plots = []
+
+for slice_name, slice_results in nih_eval_results_gender["densenet"].items():
+    plots.append(
+        go.Scatter(
+            x=pathologies,
+            y=slice_results["MultilabelAUROC"],
+            name="Overall" if slice_name == "overall" else slice_name,
+            mode="markers",
+        ),
+    )
+
+perf_metric_gender = go.Figure(data=plots)
+perf_metric_gender.update_layout(
+    title="Multilabel AUROC by Pathology and Sex",
+    title_x=0.5,
+    title_font_size=20,
+    xaxis_title="Pathology",
+    yaxis_title="Multilabel AUROC",
+    width=1024,
+    height=768,
+)
+perf_metric_gender.update_traces(
+    marker={"size": 12, "line": {"width": 2}},
+    selector={"mode": "markers"},
+)
+
+
+auroc = create_metric(
+    metric_name="auroc",
+    task="multilabel",
+    num_labels=len(pathologies),
+    thresholds=np.arange(0, 1, 0.01),
+)
+
 # define the slices
-slices_age: List[Dict[str, Dict[str, Any]]] = [
+slices = [
     {"Patient Age": {"min_value": 19, "max_value": 35}},
     {"Patient Age": {"min_value": 35, "max_value": 65}},
     {"Patient Age": {"min_value": 65, "max_value": 100}},
-    {
-        "Patient Age": {"min_value": 19, "max_value": 35},
-        "Patient Gender": {"value": "M"},
-    },
-    {
-        "Patient Age": {"min_value": 19, "max_value": 35},
-        "Patient Gender": {"value": "F"},
-    },
-    {
-        "Patient Age": {"min_value": 35, "max_value": 65},
-        "Patient Gender": {"value": "M"},
-    },
-    {
-        "Patient Age": {"min_value": 35, "max_value": 65},
-        "Patient Gender": {"value": "F"},
-    },
-    {
-        "Patient Age": {"min_value": 65, "max_value": 100},
-        "Patient Gender": {"value": "M"},
-    },
-    {
-        "Patient Age": {"min_value": 65, "max_value": 100},
-        "Patient Gender": {"value": "F"},
-    },
 ]
 
 # create the slice functions
-slice_spec = SliceSpec(spec_list=slices_age)
+slice_spec = SliceSpec(spec_list=slices)
 
 nih_eval_results_age = evaluator.evaluate(
     dataset=nih_ds,
-    metrics=[ppv, npv, sensitivity, specificity],
+    metrics=auroc,
     feature_columns="image",
     target_columns=pathologies,
     prediction_column_prefix="predictions",
     remove_columns="image",
     slice_spec=slice_spec,
+)
+
+
+# plot the results
+plots = []
+
+for slice_name, slice_results in nih_eval_results_age["densenet"].items():
+    plots.append(
+        go.Scatter(
+            x=pathologies,
+            y=slice_results["MultilabelAUROC"],
+            name="Overall" if slice_name == "overall" else slice_name,
+            mode="markers",
+        ),
+    )
+
+perf_metric_age = go.Figure(data=plots)
+perf_metric_age.update_layout(
+    title="Multilabel AUROC by Pathology and Age",
+    title_x=0.5,
+    title_font_size=20,
+    xaxis_title="Pathology",
+    yaxis_title="Multilabel AUROC",
+    width=1024,
+    height=768,
+)
+perf_metric_age.update_traces(
+    marker={"size": 12, "line": {"width": 2}},
+    selector={"mode": "markers"},
 )
 
 
@@ -221,11 +192,9 @@ fig = px.pie(
     values=[nih_ds["Patient Gender"].count("M"), nih_ds["Patient Gender"].count("F")],
     names=["Male", "Female"],
 )
-fig.update_layout(
-    title="Gender Distribution",
-    width=1200,
-    height=600,
-)
+
+fig.update_layout(title="Gender Distribution")
+
 report.log_plotly_figure(
     fig=fig,
     caption="Gender Distribution",
@@ -239,9 +208,8 @@ fig.update_layout(
     xaxis_title="Age",
     yaxis_title="Count",
     bargap=0.2,
-    width=1200,
-    height=600,
 )
+
 report.log_plotly_figure(
     fig=fig,
     caption="Age Distribution",
@@ -254,13 +222,97 @@ fig.update_layout(
     xaxis_title="Pathology",
     yaxis_title="Count",
     bargap=0.2,
-    width=1200,
-    height=600,
 )
+
 report.log_plotly_figure(
-    fig=fig,
-    caption="Pathology Distribution",
-    section_name="datasets",
+    fig=fig, caption="Pathology Distribution", section_name="datasets",
+)
+
+fig.show()
+
+
+specificity = create_metric(
+    metric_name="specificity",
+    task="multilabel",
+    num_labels=len(pathologies),
+)
+sensitivity = create_metric(
+    metric_name="sensitivity",
+    task="multilabel",
+    num_labels=len(pathologies),
+)
+
+
+fpr = 1 - specificity
+fnr = 1 - sensitivity
+
+balanced_error_rate = (fpr + fnr) / 2
+
+nih_fairness_result_age = evaluate_fairness(
+    metrics=balanced_error_rate,
+    metric_name="BalancedErrorRate",
+    dataset=nih_ds,
+    remove_columns="image",
+    target_columns=pathologies,
+    prediction_columns="predictions.densenet",
+    groups=["Patient Age"],
+    group_bins={"Patient Age": [35, 65]},
+    group_base_values={"Patient Age": 50},
+)
+
+# plot metrics per slice
+plots = []
+for slice_name, slice_results in nih_fairness_result_age.items():
+    plots.append(
+        go.Scatter(
+            x=pathologies,
+            y=slice_results["BalancedErrorRate"],
+            name=slice_name,
+            mode="markers",
+        ),
+    )
+fairness_age = go.Figure(data=plots)
+fairness_age.update_layout(
+    title="Balanced Error Rate by Age vs. Pathology",
+    title_x=0.5,
+    title_font_size=20,
+    xaxis_title="Pathology",
+    yaxis_title="Balanced Error Rate",
+    width=1024,
+    height=768,
+)
+fairness_age.update_traces(
+    marker={"size": 12, "line": {"width": 2}},
+    selector={"mode": "markers"},
+)
+
+
+# plot parity difference per slice
+plots = []
+
+for slice_name, slice_results in nih_fairness_result_age.items():
+    plots.append(
+        go.Scatter(
+            x=pathologies,
+            y=slice_results["BalancedErrorRate Parity"],
+            name=slice_name,
+            mode="markers",
+        ),
+    )
+
+fairness_age_parity = go.Figure(data=plots)
+fairness_age_parity.update_layout(
+    title="Balanced Error Rate Parity by Age vs. Pathology",
+    title_x=0.5,
+    title_font_size=20,
+    xaxis_title="Pathology",
+    yaxis_title="Balanced Error Rate Parity",
+    width=1024,
+    height=768,
+)
+fairness_age_parity.update_traces(
+    marker={"size": 12, "line": {"width": 2}},
+    selector={"mode": "markers"},
 )
 
 
@@ -269,32 +321,19 @@ for slice_, metrics in nih_eval_results_age["densenet"].items():
     for name, metric in metrics.items():
         results_flat[f"{slice_}/{name}"] = metric.mean()
         for itr, m in enumerate(metric):
-            if slice_ == "overall":
-                results_flat[f"pathology:{pathologies[itr]}/{name}"] = m
-            else:
-                results_flat[f"{slice_}&pathology:{pathologies[itr]}/{name}"] = m
+            results_flat[f"{slice_} ({pathologies[itr]})/{name}"] = m
 for slice_, metrics in nih_eval_results_gender["densenet"].items():
     for name, metric in metrics.items():
         results_flat[f"{slice_}/{name}"] = metric.mean()
         for itr, m in enumerate(metric):
-            if slice_ == "overall":
-                results_flat[f"pathology:{pathologies[itr]}/{name}"] = m
-            else:
-                results_flat[f"{slice_}&pathology:{pathologies[itr]}/{name}"] = m
+            results_flat[f"{slice_} ({pathologies[itr]})/{name} "] = m
 
 for name, metric in results_flat.items():
     split, name = name.split("/")  # noqa: PLW2901
-    descriptions = {
-        "MultilabelPositivePredictiveValue": "The proportion of correctly predicted positive instances among all instances predicted as positive. Also known as precision.",
-        "MultilabelNegativePredictiveValue": "The proportion of correctly predicted negative instances among all instances predicted as negative.",
-        "MultilabelSensitivity": "The proportion of actual positive instances that are correctly predicted. Also known as recall or true positive rate.",
-        "MultilabelSpecificity": "The proportion of actual negative instances that are correctly predicted.",
-    }
     report.log_quantitative_analysis(
         "performance",
         name=name,
         value=metric,
-        description=descriptions[name],
         metric_slice=split,
         pass_fail_thresholds=0.7,
         pass_fail_threshold_fns=lambda x, threshold: bool(x >= threshold),
@@ -345,11 +384,7 @@ report.log_citation(
     }""",
 )
 
-report.log_owner(
-    name="Machine Learning and Medicine Lab",
-    contact="mlmed.org",
-    email="mlmed@gmail.com",
-)
+report.log_owner(name="Machine Learning and Medicine Lab", contact="mlmed.org")
 
 # considerations
 report.log_user(description="Radiologists")
@@ -402,7 +437,29 @@ report.log_fairness_assessment(
         the training data is diverse and representative of the population.",
 )
 
-report_path = report.export(
-    output_filename=f"nihcxr_report_periodic_{args.synthetic_timestamp}.html",
-    synthetic_timestamp=args.synthetic_timestamp,
+
+# qualitative analysis
+report.log_plotly_figure(
+    fig=perf_metric_gender,
+    caption="MultiLabel AUROC by Pathology",
+    section_name="Quantitative Analysis",
 )
+
+report.log_plotly_figure(
+    fig=perf_metric_age,
+    caption="MultiLabel AUROC by Pathology",
+    section_name="Quantitative Analysis",
+)
+
+report.log_plotly_figure(
+    fig=fairness_age,
+    caption="Balanced Error Rate by Age vs. Pathology",
+    section_name="Fairness Analysis",
+)
+report.log_plotly_figure(
+    fig=fairness_age_parity,
+    caption="Balanced Error Rate Parity by Age vs. Pathology",
+    section_name="Fairness Analysis",
+)
+
+report_path = report.export()
