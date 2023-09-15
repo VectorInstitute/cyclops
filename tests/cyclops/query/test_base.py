a48943b77708cf25a149bc47ec449616078518ba
Amrit K <amritk@vectorinstitute.ai>
2023-09-15 10:22:29 -0400

    Update test for list_tables

--- tests/cyclops/query/test_base.py
+++ tests/cyclops/query/test_base.py
@@ -10,6 +10,8 @@ def test_dataset_querier():
     """Test base querier methods using OMOPQuerier."""
     querier = OMOPQuerier("cdm_synthea10", database="synthea_integration_test")
     assert len(querier.list_tables()) == 69
+    assert len(querier.list_schemas()) == 4
+    assert len(querier.list_tables(schema_name="cdm_synthea10")) == 43
     visit_occrrence_columns = querier.list_columns("cdm_synthea10", "visit_occurrence")
     assert len(visit_occrrence_columns) == 17
     assert "visit_occurrence_id" in visit_occrrence_columns
