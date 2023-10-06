9b03aeb8a960d9377284f274fdf4818fa93a1e25
Amrit K <amritk@vectorinstitute.ai>
2023-10-06 09:41:16 -0400

    Apply ruff formatting fix

--- cyclops/tasks/__init__.py
+++ cyclops/tasks/__init__.py
@@ -1,3 +1,3 @@
 """Tasks package."""
-from cyclops.tasks.mortality_prediction import MortalityPredictionTask
 from cyclops.tasks.cxr_classification import CXRClassificationTask
+from cyclops.tasks.mortality_prediction import MortalityPredictionTask
