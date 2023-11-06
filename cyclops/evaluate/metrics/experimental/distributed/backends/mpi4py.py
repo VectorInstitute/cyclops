fe6b1ca50ed4145f83b095bc803ab0fbb7193e7c
Franklin <41602287+fcogidi@users.noreply.github.com>
2023-11-06 16:29:21 -0500

    Add distributed backends package

---
+++ cyclops/evaluate/metrics/experimental/distributed/backends/mpi4py.py
@@ -0,0 +1,104 @@
+"""mpi4py backend for synchronizing array-API-compatible objects."""
+import os
+from typing import TYPE_CHECKING, List
+
+import array_api_compat as apc
+
+from cyclops.evaluate.metrics.experimental.distributed.backends.base import (
+    DistributedBackend,
+)
+from cyclops.evaluate.metrics.experimental.utils.ops import flatten
+from cyclops.evaluate.metrics.experimental.utils.typing import Array
+from cyclops.utils.optional import import_optional_module
+
+
+if TYPE_CHECKING:
+    from mpi4py import MPI
+else:
+    MPI = import_optional_module("mpi4py.MPI", error="ignore")
+
+
+class MPI4Py(DistributedBackend, registry_key="mpi4py"):
+    """A distributed communication backend for mpi4py."""
+
+    def __init__(self) -> None:
+        """Initialize the MPI4Py backend."""
+        super().__init__()
+        if MPI is None:
+            raise ImportError(
+                f"For availability of {self.__class__.__name__},"
+                " please install mpi4py first.",
+            )
+
+    @property
+    def is_initialized(self) -> bool:
+        """Return `True` if the distributed environment has been initialized."""
+        return "OMPI_COMM_WORLD_SIZE" in os.environ
+
+    @property
+    def rank(self) -> int:
+        """Return the rank of the current process."""
+        comm = MPI.COMM_WORLD
+        return comm.Get_rank()
+
+    @property
+    def world_size(self) -> int:
+        """Return the total number of processes."""
+        comm = MPI.COMM_WORLD
+        return comm.Get_size()
+
+    def all_gather(self, arr: Array) -> List[Array]:
+        """Gather Arrays from current proccess and return as a list.
+
+        Parameters
+        ----------
+        arr : Array
+            Any array-API-compatible object.
+
+        Returns
+        -------
+        List[Array]
+            A list of the gathered array-API-compatible objects.
+        """
+        try:
+            xp = apc.array_namespace(arr)
+        except TypeError as e:
+            raise TypeError(
+                "The given array is not array-API-compatible. "
+                "Please use array-API-compatible objects.",
+            ) from e
+
+        comm = MPI.COMM_WORLD
+
+        # gather the shape and size of each array
+        local_shape = arr.shape
+        local_size = apc.size(arr)
+        all_shapes = comm.allgather(local_shape)
+        all_sizes = comm.allgather(local_size)
+
+        # prepare displacements for `Allgatherv``
+        displacements = [0]
+        for shape in all_shapes[:-1]:
+            shape_arr = xp.asarray(shape)
+            displacements.append(displacements[-1] + int(xp.prod(shape_arr)))
+
+        # allocate memory for gathered data based on total size
+        total_size = sum([int(xp.prod(xp.asarray(shape))) for shape in all_shapes])
+        gathered_data = xp.empty(total_size, dtype=arr.dtype)
+
+        # gather data from all processes to all processes, accounting for uneven shapes
+        comm.Allgatherv(
+            flatten(arr),
+            [gathered_data, (all_sizes, displacements)],
+        )
+
+        # reshape gathered data back to original shape
+        reshaped_data = []
+        for shape in all_shapes:
+            shape_arr = xp.asarray(shape)
+            reshaped_data.append(
+                xp.reshape(gathered_data[: xp.prod(shape_arr)], shape=shape),
+            )
+            gathered_data = gathered_data[xp.prod(shape_arr) :]
+
+        return reshaped_data
---
+++ cyclops/evaluate/metrics/experimental/distributed/backends/torch_distributed.py
@@ -0,0 +1,99 @@
+"""`torch.distributed` backend for synchronizing `torch.Tensor` objects."""
+from typing import TYPE_CHECKING, List, TypeVar
+
+from cyclops.evaluate.metrics.experimental.distributed.backends.base import (
+    DistributedBackend,
+)
+from cyclops.utils.optional import import_optional_module
+
+
+if TYPE_CHECKING:
+    import torch
+    import torch.distributed as torch_dist
+else:
+    torch = import_optional_module("torch", error="ignore")
+    torch_dist = import_optional_module("torch.distributed", error="ignore")
+
+Tensor = TypeVar("Tensor", bound="torch.Tensor")
+
+
+class TorchDistributed(DistributedBackend, registry_key="torch_distributed"):
+    """A distributed communication backend for torch.distributed."""
+
+    def __init__(self) -> None:
+        """Initialize the object."""
+        super().__init__()
+        if torch is None:
+            raise ImportError(
+                f"For availability of {self.__class__.__name__},"
+                " please install pytorch first.",
+            )
+        if not torch_dist.is_available():
+            raise RuntimeError(
+                f"For availability of {self.__class__.__name__},"
+                " make sure torch.distributed is available.",
+            )
+
+    @property
+    def is_initialized(self) -> bool:
+        """Return `True` if the distributed environment has been initialized."""
+        return torch_dist.is_initialized()
+
+    @property
+    def rank(self) -> int:
+        """Return the rank of the current process group."""
+        return torch_dist.get_rank()
+
+    @property
+    def world_size(self) -> int:
+        """Return the world size of the current process group."""
+        return torch_dist.get_world_size()
+
+    def _simple_all_gather(self, data: torch.Tensor) -> List[torch.Tensor]:
+        """Gather tensors of the same shape from all processes."""
+        gathered_data = [torch.zeros_like(data) for _ in range(self.world_size)]
+        torch_dist.all_gather(gathered_data, data)  # type: ignore[no-untyped-call]
+        return gathered_data
+
+    def all_gather(self, data: torch.Tensor) -> List[torch.Tensor]:
+        """Gather Arrays from current proccess and return as a list.
+
+        Parameters
+        ----------
+        arr : torch.Tensor
+            `torch.Tensor` object to be gathered.
+
+        Returns
+        -------
+        List[Array]
+            A list of the gathered `torch.Tensor` objects.
+        """
+        data = data.contiguous()
+
+        if data.ndim == 0:
+            return self._simple_all_gather(data)
+
+        # gather sizes of all tensors
+        local_size = torch.tensor(data.shape, device=data.device)
+        local_sizes = [torch.zeros_like(local_size) for _ in range(self.world_size)]
+        torch_dist.all_gather(local_sizes, local_size)  # type: ignore[no-untyped-call]
+        max_size = torch.stack(local_sizes).max(dim=0).values
+        all_sizes_equal = all(all(ls == max_size) for ls in local_sizes)
+
+        # if shapes are all the same, do a simple gather
+        if all_sizes_equal:
+            return self._simple_all_gather(data)
+
+        # if not, pad each local tensor to maximum size, gather and then truncate
+        pad_dims = []
+        pad_by = (max_size - local_size).detach().cpu()
+        for val in reversed(pad_by):
+            pad_dims.append(0)
+            pad_dims.append(val.item())
+        data_padded = torch.nn.functional.pad(data, pad_dims)
+        gathered_data = [torch.zeros_like(data_padded) for _ in range(self.world_size)]
+        torch_dist.all_gather(gathered_data, data_padded)  # type: ignore[no-untyped-call]
+        for idx, item_size in enumerate(local_sizes):
+            slice_param = [slice(dim_size) for dim_size in item_size]
+            gathered_data[idx] = gathered_data[idx][slice_param]
+        return gathered_data
