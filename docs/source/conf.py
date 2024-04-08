09baa261d2ab894351dc47f81731520dc90e5aea
Amrit K <amritk@vectorinstitute.ai>
2024-04-08 09:14:03 -0400

    Include js files in sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,6 +94,10 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_js_files = [
+    "require.min.js",
+    "custom.js",
+]
 html_css_files = ["css/cyclops.css"]
 html_logo = "_static/cyclops_logo-dark.png"
 html_favicon = "_static/favicon.ico"
