2fd1f7281eab1ddb2f807bdbc2084614eef05115
akore <akore0x5f@gmail.com>
2024-07-24 14:31:05 -0400

    fix mypy error

--- cyclops/tasks/classification.py
+++ cyclops/tasks/classification.py
@@ -413,7 +413,7 @@ class MultilabelImageClassificationTask(BaseTask):
     def _validate_models(self) -> None:
         """Validate the models for the task data type."""
         assert all(
-            _model_names_mapping.get(model.model.__name__) in _img_model_keys
+            _model_names_mapping.get(model.model.__name__) in _img_model_keys  # type: ignore
             for model in self.models.values()
         ), "All models must be image type model."
         for model in self.models.values():
